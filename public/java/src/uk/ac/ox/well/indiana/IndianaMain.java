4ad27dbae5d14a76eff60adbd37cc5b77e1a3d96
Kiran V Garimella <kiran@well.ox.ac.uk>
2012-09-05 17:41:36 +0100

    Improvements to command-line argument parsing

--- public/java/src/uk/ac/ox/well/indiana/IndianaMain.java
+++ public/java/src/uk/ac/ox/well/indiana/IndianaMain.java
@@ -8,6 +8,7 @@ import uk.ac.ox.well.indiana.tools.Tool;
 import uk.ac.ox.well.indiana.utils.arguments.Argument;
 import uk.ac.ox.well.indiana.utils.packageutils.PackageInspector;
 
+import java.io.File;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
@@ -17,9 +18,9 @@ import java.util.HashMap;
 import java.util.Set;
 
 public class IndianaMain {
-    public static void main(String[] args) throws IndianaException {
+    public static void main(String[] args) throws Exception {
         if (args.length == 0 || args[0].equals("-h") || args[0].equals("--help") || args[0].equals("-H")) {
-            showHelp();
+            showPrimaryHelp();
         } else if (args.length > 0) {
             String toolName = args[0];
             String[] toolArgs = Arrays.copyOfRange(args, 1, args.length);
@@ -27,10 +28,8 @@ public class IndianaMain {
             HashMap<String, Class<? extends Tool>> tools = new PackageInspector<Tool>(Tool.class).getExtendingClassesMap();
 
             if (tools.containsKey(toolName)) {
-                Class<? extends Tool> tool = tools.get(toolName);
-
                 try {
-                    Tool instance = tool.newInstance();
+                    Tool instance = tools.get(toolName).newInstance();
                     Options options = new Options();
 
                     Field[] fields = instance.getClass().getDeclaredFields();
@@ -40,38 +39,67 @@ public class IndianaMain {
                             if (annotation.annotationType().equals(Argument.class)) {
                                 Argument arg = (Argument) annotation;
 
-                                options.addOption(arg.shortName(), arg.fullName(), arg.required(), arg.doc());
+                                String description = arg.doc() + " [default: " + field.get(instance) + "]";
+                                Boolean hasArgument = !field.getType().equals(Boolean.class);
+
+                                Option option = new Option(arg.shortName(), arg.fullName(), hasArgument, description);
+                                option.setType(field.getType());
+                                options.addOption(option);
                             }
                         }
                     }
 
+                    options.addOption("h", "help", false, "Show this help message");
+
                     CommandLineParser parser = new PosixParser();
                     CommandLine cmd = parser.parse(options, toolArgs);
-
-                    /*
-                    HelpFormatter formatter = new HelpFormatter();
-                    formatter.printHelp("java -jar indiana.jar " + toolName, options);
-
-                    Option[] parsedOptions = cmd.getOptions();
-                    for (Option option : parsedOptions) {
-                        System.out.println(option + " " + option.getValue());
-                    }
-                    */
 
                     for (Field field : fields) {
                         for (Annotation annotation : field.getDeclaredAnnotations()) {
                             if (annotation.annotationType().equals(Argument.class)) {
                                 Argument arg = (Argument) annotation;
 
-                                String value = cmd.getOptionValue(arg.fullName());
-                                field.set(instance, value);
+                                if (field.getType().equals(Boolean.class)) {
+                                    if (cmd.hasOption(arg.fullName())) {
+                                        Boolean prevValue = (Boolean) field.get(instance);
+                                        field.set(instance, !prevValue);
+                                    }
+                                } else {
+                                    String value = cmd.getOptionValue(arg.fullName());
+
+                                    if (value != null) {
+                                        Class<?> type = field.getType();
+
+                                        if (type.equals(File.class)) {
+                                            field.set(instance, new File(value));
+                                        } else if (type.equals(Integer.class)) {
+                                            field.set(instance, Integer.valueOf(value));
+                                        } else if (type.equals(Float.class)) {
+                                            field.set(instance, Float.valueOf(value));
+                                        } else if (type.equals(Double.class)) {
+                                            field.set(instance, Double.valueOf(value));
+                                        } else if (type.equals(String.class)) {
+                                            field.set(instance, value);
+                                        } else {
+                                        }
+                                    } else if (arg.required()) {
+                                        // do something else here
+                                    }
+                                }
                             }
                         }
                     }
 
-                    Method method = instance.getClass().getMethod("execute");
+                    if (cmd.hasOption("help") || toolArgs.length == 0) {
+                        HelpFormatter formatter = new HelpFormatter();
+                        formatter.printHelp("java -jar indiana.jar " + toolName + " [options]", options);
+                        System.exit(1);
+                    }
 
-                    method.invoke(instance);
+                    Method method = instance.getClass().getMethod("execute");
+                    Integer result = (Integer) method.invoke(instance);
+
+                    System.exit(result);
                 } catch (InstantiationException e) {
                     throw new ToolResolutionException(toolName, e);
                 } catch (IllegalAccessException e) {
@@ -84,12 +112,12 @@ public class IndianaMain {
                     e.printStackTrace();
                 }
             } else {
-                throw new ToolNotFoundException(toolName);
+                showInvalidToolMessage(toolName);
             }
         }
     }
 
-    private static void showHelp() {
+    private static void showPrimaryHelp() {
         Set<Class<? extends Tool>> tools = new PackageInspector<Tool>(Tool.class).getExtendingClasses();
 
         System.out.println("usage: java -jar indiana.jar [-h|--help|-H]");
@@ -101,5 +129,12 @@ public class IndianaMain {
             System.out.println("   " + t.getSimpleName());
         }
         System.out.println();
+
+        System.exit(1);
+    }
+
+    private static void showInvalidToolMessage(String tool) {
+        System.out.println("indiana: '" + tool + "' is not an INDIANA tool. See 'java -jar indiana.jar --help'.");
+        System.exit(1);
     }
 }
