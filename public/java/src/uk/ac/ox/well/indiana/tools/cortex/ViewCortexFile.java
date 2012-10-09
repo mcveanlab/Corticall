6822678bd3d586b64f8af8e807e1f3d4f40f43c3
Kiran V Garimella <kiran@well.ox.ac.uk>
2012-10-09 16:19:01 +0100

    Added a tool to extract transcripts (defined in a GFF file) from a Fasta file. Added BioJava to dependencies.

--- public/java/src/uk/ac/ox/well/indiana/tools/cortex/ViewCortexFile.java
+++ public/java/src/uk/ac/ox/well/indiana/tools/cortex/ViewCortexFile.java
@@ -2,17 +2,30 @@ package uk.ac.ox.well.indiana.tools.cortex;
 
 import uk.ac.ox.well.indiana.tools.Tool;
 import uk.ac.ox.well.indiana.utils.arguments.Argument;
+import uk.ac.ox.well.indiana.utils.arguments.Output;
 import uk.ac.ox.well.indiana.utils.io.cortex.CortexGraph;
 import uk.ac.ox.well.indiana.utils.io.cortex.CortexRecord;
 
+import java.io.PrintStream;
+
 public class ViewCortexFile extends Tool {
-    @Argument(fullName="cg", shortName="c", doc="Cortex graph")
+    @Argument(fullName="cortexFile", shortName="c", doc="Cortex graph")
     public CortexGraph CORTEX_GRAPH;
+
+    @Argument(fullName="headerOnly", shortName="H", doc="Only print the header")
+    public Boolean HEADER_ONLY = false;
+
+    @Output
+    public PrintStream out;
 
     @Override
     public int execute() {
-        for (CortexRecord cr : CORTEX_GRAPH) {
-            System.out.println(cr);
+        if (HEADER_ONLY) {
+            out.println(CORTEX_GRAPH);
+        } else {
+            for (CortexRecord cr : CORTEX_GRAPH) {
+                out.println(cr);
+            }
         }
 
         return 0;
---
+++ public/java/src/uk/ac/ox/well/indiana/tools/sequence/ExtractTranscriptsFromFasta.java
@@ -0,0 +1,118 @@
+package uk.ac.ox.well.indiana.tools.sequence;
+
+import net.sf.picard.reference.FastaSequenceFile;
+import net.sf.picard.reference.ReferenceSequence;
+import uk.ac.ox.well.indiana.tools.Tool;
+import uk.ac.ox.well.indiana.utils.arguments.Argument;
+import uk.ac.ox.well.indiana.utils.arguments.Output;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class ExtractTranscriptsFromFasta extends Tool {
+    @Argument(fullName="fasta", shortName="f", doc="Fasta file from which sequences should be extracted")
+    public FastaSequenceFile FASTA;
+
+    @Argument(fullName="gff", shortName="gff", doc="The GFF file with the transcript definitions")
+    public File REGIONS;
+
+    @Output
+    public PrintStream out;
+
+    private class Transcript {
+        String name;
+        String contig;
+        int start;
+        int stop;
+
+        public Transcript(String name, String contig, int start, int stop) {
+            this.name = name;
+            this.contig = contig;
+            this.start = start;
+            this.stop = stop;
+        }
+
+        public String getName() { return name; }
+
+        public String getContig() { return contig; }
+
+        public int getStart() { return start; }
+
+        public int getStop() { return stop; }
+
+        public String toString() {
+            return getName() + " " + getContig() + ":" + getStart() + "-" + getStop();
+        }
+    }
+
+    private HashMap<String, ArrayList<Transcript>> loadGFFFile(File regions) {
+        HashMap<String, ArrayList<Transcript>> transcripts = new HashMap<String, ArrayList<Transcript>>();
+
+        try {
+            BufferedReader gffReader = new BufferedReader(new FileReader(regions));
+
+            String line;
+            while ((line = gffReader.readLine()) != null) {
+                if (!line.startsWith("#") && line.contains("gene")) {
+                    String[] fields = line.split("\\s+");
+                    String contigName = fields[0];
+                    int start = Integer.valueOf(fields[3]);
+                    int stop = Integer.valueOf(fields[4]);
+
+                    String transcriptName = "unknown";
+
+                    String[] info = fields[8].split(";");
+                    for (String infoField : info) {
+                        if (infoField.startsWith("ID=")) {
+                            String[] infoFieldPieces = infoField.split("=");
+
+                            transcriptName = infoFieldPieces[1];
+                        }
+                    }
+
+                    Transcript t = new Transcript(transcriptName, contigName, start, stop);
+
+                    if (!transcripts.containsKey(contigName)) {
+                        transcripts.put(contigName, new ArrayList<Transcript>());
+                    }
+
+                    transcripts.get(contigName).add(t);
+                }
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return transcripts;
+    }
+
+    @Override
+    public int execute() {
+        HashMap<String, ArrayList<Transcript>> transcripts = loadGFFFile(REGIONS);
+
+        ReferenceSequence contig;
+        while ((contig = FASTA.nextSequence()) != null) {
+            String[] name = contig.getName().split("\\s+");
+            String contigName = name[0];
+
+            byte[] bases = contig.getBases();
+
+            if (transcripts.containsKey(contigName)) {
+                for (Transcript t : transcripts.get(contigName)) {
+                    //out.println(t);
+
+                    byte[] transcriptBases = Arrays.copyOfRange(bases, t.getStart()-1, t.getStop());
+
+                    out.println(">" + t.getName());
+                    out.println(new String(transcriptBases));
+                }
+            }
+        }
+
+        return 0;
+    }
+}
