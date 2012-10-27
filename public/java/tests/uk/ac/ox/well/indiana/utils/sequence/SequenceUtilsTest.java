0bc054fc85decfae349baf6a636599d156148594
Kiran V Garimella <kiran@well.ox.ac.uk>
2012-10-27 17:32:17 +0100

    TestNG now works without the need for a testng.xml file (dynamically locates tests to run).  Added 'release' ant target that creates a TestNG-tested jar file with a version number (4-digits from the SHA1 code of the latest git commit)

---
+++ public/java/tests/uk/ac/ox/well/indiana/utils/sequence/SequenceUtilsTest.java
@@ -0,0 +1,10 @@
+package uk.ac.ox.well.indiana.utils.sequence;
+
+import org.testng.annotations.Test;
+
+public class SequenceUtilsTest {
+    @Test(groups = { "basic" })
+    public void getReverseComplementTest() {
+        System.out.println("This is a test.");
+    }
+}
