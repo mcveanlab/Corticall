509b2ad702620e3050d03c8e5873e59edd5081a8
Kiran V Garimella <kiran@well.ox.ac.uk>
2013-04-02 15:40:42 +0100

    Added a memory performance string that's shorter than the existing one (makes printing prettier)

--- public/java/src/uk/ac/ox/well/indiana/utils/performance/PerformanceUtils.java
+++ public/java/src/uk/ac/ox/well/indiana/utils/performance/PerformanceUtils.java
@@ -15,4 +15,17 @@ public class PerformanceUtils {
 
         return String.format("Used: %dmb; Free: %dmb; Total: %dmb; Max: %dmb", usedMemory, freeMemory, totalMemory, maxMemory);
     }
+
+    public static String getCompactMemoryUsageStats() {
+        int mb = 1024*1024;
+
+        Runtime runtime = Runtime.getRuntime();
+
+        long usedMemory = (runtime.totalMemory() - runtime.freeMemory()) / mb;
+        long freeMemory = runtime.freeMemory() / mb;
+        long totalMemory = runtime.totalMemory() / mb;
+        long maxMemory = runtime.maxMemory() / mb;
+
+        return String.format("U:%dmb; F:%dmb; T:%dmb; M:%dmb", usedMemory, freeMemory, totalMemory, maxMemory);
+    }
 }
