69428818ca587fb882a23edecda8684752214f89
Kiran V Garimella <kiran@well.ox.ac.uk>
2013-04-02 15:43:11 +0100

    Some basic, reuseable sketch elements

---
+++ public/java/src/uk/ac/ox/well/indiana/utils/processing/visualelements/Frame.java
@@ -0,0 +1,6 @@
+package uk.ac.ox.well.indiana.utils.processing.visualelements;
+
+public abstract class Frame implements VisualElement {
+    @Override
+    public abstract void draw();
+}
---
+++ public/java/src/uk/ac/ox/well/indiana/utils/processing/visualelements/VisualElement.java
@@ -0,0 +1,8 @@
+package uk.ac.ox.well.indiana.utils.processing.visualelements;
+
+public interface VisualElement {
+    public void draw();
+
+    public int getWidth();
+    public int getHeight();
+}
