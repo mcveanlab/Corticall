77d6e78449a475e6058f4747817a4a5908cf82c1
Kiran V Garimella <kiran@well.ox.ac.uk>
2013-05-21 16:07:40 +0100

    Added a simple tool to pull out sequences from a FASTA file based on gene entries in a GFF file

---
+++ public/java/src/uk/ac/ox/well/indiana/analyses/reconstruction/ExtractGenesFromFasta.java
@@ -0,0 +1,33 @@
+package uk.ac.ox.well.indiana.analyses.reconstruction;
+
+import net.sf.picard.reference.IndexedFastaSequenceFile;
+import uk.ac.ox.well.indiana.tools.Tool;
+import uk.ac.ox.well.indiana.utils.arguments.Argument;
+import uk.ac.ox.well.indiana.utils.arguments.Output;
+import uk.ac.ox.well.indiana.utils.io.gff.GFF3;
+import uk.ac.ox.well.indiana.utils.io.gff.GFF3Record;
+
+import java.io.PrintStream;
+
+public class ExtractGenesFromFasta extends Tool {
+    @Argument(fullName="reference", shortName="R", doc="Reference sequence")
+    public IndexedFastaSequenceFile REFERENCE;
+
+    @Argument(fullName="gff", shortName="gff", doc="GFF file")
+    public GFF3 GFF;
+
+    @Output
+    public PrintStream out;
+
+    @Override
+    public void execute() {
+        for (GFF3Record gr : GFF) {
+            if ("gene".equals(gr.getType())) {
+                String seq = new String(REFERENCE.getSubsequenceAt(gr.getSeqid(), gr.getStart(), gr.getEnd()).getBases());
+
+                out.println(">" + gr.getAttribute("ID"));
+                out.println(seq);
+            }
+        }
+    }
+}
