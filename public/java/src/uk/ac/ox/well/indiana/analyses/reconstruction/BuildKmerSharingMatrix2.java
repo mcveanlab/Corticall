12337b5266f5e927126a472dfe2b5ba16f949180
Kiran V Garimella <kiran@well.ox.ac.uk>
2013-06-06 14:39:56 +0100

    New kmer sharing matrix tool

--- public/java/src/uk/ac/ox/well/indiana/analyses/reconstruction/BuildKmerSharingMatrix2.java
+++ public/java/src/uk/ac/ox/well/indiana/analyses/reconstruction/BuildKmerSharingMatrix2.java
@@ -25,17 +25,17 @@ public class BuildKmerSharingMatrix2 extends Tool {
 
     @Override
     public void execute() {
+        Map<String, Integer> sampleIndex = new TreeMap<String, Integer>();
+        Map<Integer, String> indexSample = new HashMap<Integer, String>();
+        for (int i = 0; i < SAMPLES.size(); i++) {
+            sampleIndex.put(SAMPLES.get(i), i);
+            indexSample.put(i, SAMPLES.get(i));
+        }
+
+        Map<CortexKmer, boolean[]> kmerPresence = new HashMap<CortexKmer, boolean[]>();
+
+        /*
         try {
-            Map<String, Integer> sampleIndex = new TreeMap<String, Integer>();
-            Map<Integer, String> indexSample = new HashMap<Integer, String>();
-            for (int i = 0; i < SAMPLES.size(); i++) {
-                sampleIndex.put(SAMPLES.get(i), i);
-                indexSample.put(i, SAMPLES.get(i));
-            }
-
-            Map<CortexKmer, boolean[]> kmerPresence = new HashMap<CortexKmer, boolean[]>();
-
-            /*
             BufferedReader br = new BufferedReader(new FileReader(CONTIG_TABLE));
 
             String[] header = br.readLine().split("\t");
@@ -76,83 +76,83 @@ public class BuildKmerSharingMatrix2 extends Tool {
                     kmerPresence.get(kmer)[sindex] = true;
                 }
             }
-            */
-
-            TableReader tr = new TableReader(CONTIG_TABLE);
-            for (Map<String, String> entry : tr) {
-                CortexKmer contig = new CortexKmer(entry.get("contig"));
-
-                String[] kmers = entry.get("kmers").split(",");
-                int kmerSize = kmers[0].length();
-
-                String sample = entry.get("sample");
-                int sindex = sampleIndex.get(sample);
-
-                for (int i = 0; i < contig.length() - kmerSize; i++) {
-                    CortexKmer kmer = contig.getSubKmer(i, kmerSize);
-
-                    if (!kmerPresence.containsKey(kmer)) {
-                        kmerPresence.put(kmer, new boolean[SAMPLES.size()]);
-
-                        for (int j = 0; j < SAMPLES.size(); j++) {
-                            kmerPresence.get(kmer)[j] = false;
-                        }
-                    }
-
-                    kmerPresence.get(kmer)[sindex] = true;
-                }
-            }
-
-            Map<String, Map<String, Integer>> data = new HashMap<String, Map<String, Integer>>();
-            for (String sample1 : SAMPLES) {
-                data.put(sample1, new HashMap<String, Integer>());
-
-                for (String sample2 : SAMPLES) {
-                    data.get(sample1).put(sample2, 0);
-                }
-            }
-
-            for (CortexKmer kmer : kmerPresence.keySet()) {
-                boolean[] presence = kmerPresence.get(kmer);
-
-                for (int i = 0; i < presence.length; i++) {
-                    String sample1 = indexSample.get(i);
-
-                    if (presence[i]) {
-                        for (int j = 0; j < presence.length; j++) {
-                            String sample2 = indexSample.get(j);
-
-                            if (presence[j]) {
-                                int count = data.get(sample1).get(sample2);
-                                data.get(sample1).put(sample2, count + 1);
-                            }
-                        }
-                    }
-                }
-            }
-
-            List<String> mheader = new ArrayList<String>();
-            mheader.add("\t");
-            for (String sample : data.keySet()) {
-                mheader.add(sample);
-            }
-
-            out.println(Joiner.on("\t").join(mheader));
-
-            for (String sample1 : data.keySet()) {
-                List<String> row = new ArrayList<String>();
-                row.add(sample1);
-
-                for (String sample2 : data.keySet()) {
-                    row.add(String.valueOf(data.get(sample1).get(sample2)));
-                }
-
-                out.println(Joiner.on("\t").join(row));
-            }
         } catch (FileNotFoundException e) {
             throw new RuntimeException("Could not open file '" + CONTIG_TABLE.getAbsolutePath() + "': " + e);
         } catch (IOException e) {
             throw new RuntimeException("Could not read line from '" + CONTIG_TABLE.getAbsolutePath() + "': " + e);
+        }
+        */
+
+        TableReader tr = new TableReader(CONTIG_TABLE);
+        for (Map<String, String> entry : tr) {
+            CortexKmer contig = new CortexKmer(entry.get("contig"));
+
+            String[] kmers = entry.get("kmers").split(",");
+            int kmerSize = kmers[0].length();
+
+            String sample = entry.get("sample");
+            int sindex = sampleIndex.get(sample);
+
+            for (int i = 0; i < contig.length() - kmerSize; i++) {
+                CortexKmer kmer = contig.getSubKmer(i, kmerSize);
+
+                if (!kmerPresence.containsKey(kmer)) {
+                    kmerPresence.put(kmer, new boolean[SAMPLES.size()]);
+
+                    for (int j = 0; j < SAMPLES.size(); j++) {
+                        kmerPresence.get(kmer)[j] = false;
+                    }
+                }
+
+                kmerPresence.get(kmer)[sindex] = true;
+            }
+        }
+
+        Map<String, Map<String, Integer>> data = new HashMap<String, Map<String, Integer>>();
+        for (String sample1 : SAMPLES) {
+            data.put(sample1, new HashMap<String, Integer>());
+
+            for (String sample2 : SAMPLES) {
+                data.get(sample1).put(sample2, 0);
+            }
+        }
+
+        for (CortexKmer kmer : kmerPresence.keySet()) {
+            boolean[] presence = kmerPresence.get(kmer);
+
+            for (int i = 0; i < presence.length; i++) {
+                String sample1 = indexSample.get(i);
+
+                if (presence[i]) {
+                    for (int j = 0; j < presence.length; j++) {
+                        String sample2 = indexSample.get(j);
+
+                        if (presence[j]) {
+                            int count = data.get(sample1).get(sample2);
+                            data.get(sample1).put(sample2, count + 1);
+                        }
+                    }
+                }
+            }
+        }
+
+        List<String> mheader = new ArrayList<String>();
+        mheader.add("\t");
+        for (String sample : data.keySet()) {
+            mheader.add(sample);
+        }
+
+        out.println(Joiner.on("\t").join(mheader));
+
+        for (String sample1 : data.keySet()) {
+            List<String> row = new ArrayList<String>();
+            row.add(sample1);
+
+            for (String sample2 : data.keySet()) {
+                row.add(String.valueOf(data.get(sample1).get(sample2)));
+            }
+
+            out.println(Joiner.on("\t").join(row));
         }
     }
 }
