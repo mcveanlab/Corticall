ac30884b902c2728700ba04713734f98c751fda0
Kiran V Garimella <kiran@well.ox.ac.uk>
2013-11-09 15:37:21 +0000

    Color edges based on regular expressions

--- public/java/src/uk/ac/ox/well/indiana/analyses/roi/VisualizeSequenceTree.java
+++ public/java/src/uk/ac/ox/well/indiana/analyses/roi/VisualizeSequenceTree.java
@@ -1,5 +1,6 @@
 package uk.ac.ox.well.indiana.analyses.roi;
 
+import com.google.common.base.Joiner;
 import net.sf.picard.reference.FastaSequenceFile;
 import net.sf.picard.reference.ReferenceSequence;
 import org.jgrapht.DirectedGraph;
@@ -17,10 +18,7 @@ import uk.ac.ox.well.indiana.utils.io.jgrapht.ExtendedDOTExporter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class VisualizeSequenceTree extends Module {
     @Argument(fullName="sequences", shortName="s", doc="Sequences FASTA")
@@ -31,6 +29,9 @@ public class VisualizeSequenceTree extends Module {
 
     @Argument(fullName="skipSimplification", shortName="ss", doc="Don't simplify the graph")
     public Boolean SKIP_SIMPLIFICATION = false;
+
+    @Argument(fullName="highlight", shortName="hl", doc="Colon-separated regex:color list (e.g. 'Pf3D7:#ff0000') specifying the sequence(s) to highlight with the specified color.", required=false)
+    public ArrayList<String> HIGHLIGHT;
 
     @Output
     public File out;
@@ -51,8 +52,8 @@ public class VisualizeSequenceTree extends Module {
             Map<String, String> attrs = new HashMap<String, String>();
 //            attrs.put("shape", "rect");
             attrs.put("shape", "circle");
-            attrs.put("height", "0.12");
-            attrs.put("width", "0.12");
+            //attrs.put("height", "0.12");
+            attrs.put("width", String.valueOf(Math.log10(s.length() - KMER_SIZE + 1)/10.0 + 0.12));
             attrs.put("fontsize", "1");
 
             return attrs;
@@ -60,18 +61,27 @@ public class VisualizeSequenceTree extends Module {
     }
 
     public class CustomEdgeAttributeProvider implements ComponentAttributeProvider<DefaultEdge> {
-        private Map<DefaultEdge, Integer> edgeWeights = new HashMap<DefaultEdge, Integer>();
+        private Map<DefaultEdge, Map<String, Integer>> edgeWeights = new HashMap<DefaultEdge, Map<String, Integer>>();
+        private Map<DefaultEdge, Set<String>> edgeColors = new HashMap<DefaultEdge, Set<String>>();
 
-        public void incrementWeight(DefaultEdge edge) {
-            if (edgeWeights.containsKey(edge)) {
-                edgeWeights.put(edge, edgeWeights.get(edge) + 2);
+        public void incrementWeight(DefaultEdge edge, String color) {
+            if (!edgeWeights.containsKey(edge)) {
+                edgeWeights.put(edge, new HashMap<String, Integer>());
+            }
+
+            if (!edgeWeights.get(edge).containsKey(color)) {
+                edgeWeights.get(edge).put(color, 1);
             } else {
-                edgeWeights.put(edge, 1);
+                edgeWeights.get(edge).put(color, edgeWeights.get(edge).get(color) + 2);
             }
         }
 
-        public int getWeight(DefaultEdge edge) {
-            return edgeWeights.containsKey(edge) ? edgeWeights.get(edge) : 0;
+        public void addColor(DefaultEdge edge, String color) {
+            if (!edgeColors.containsKey(edge)) {
+                edgeColors.put(edge, new HashSet<String>());
+            }
+
+            edgeColors.get(edge).add(color);
         }
 
         @Override
@@ -79,122 +89,33 @@ public class VisualizeSequenceTree extends Module {
             Map<String, String> attrs = new HashMap<String, String>();
             attrs.put("arrowhead", "none");
 
-            if (edgeWeights.containsKey(edge)) {
-                attrs.put("penwidth", String.valueOf(edgeWeights.get(edge)));
+            //if (edgeWeights.containsKey(edge)) {
+            //    attrs.put("penwidth", String.valueOf(edgeWeights.get(edge)));
+            //}
+
+            Map<String, Integer> colorWidths = new HashMap<String, Integer>();
+
+            if (edgeColors.containsKey(edge)) {
+                attrs.put("color", Joiner.on(":").join(edgeColors.get(edge)));
+
+                for (String color : edgeColors.get(edge)) {
+                    if (!colorWidths.containsKey(color)) {
+                        colorWidths.put(color, 1);
+                    } else {
+                        colorWidths.put(color, colorWidths.get(color) + 2);
+                    }
+                }
+
+                List<Integer> widths = new ArrayList<Integer>();
+                for (String color : edgeColors.get(edge)) {
+                    widths.add(edgeWeights.get(edge).get(color));
+                }
+
+                attrs.put("penwidth", Joiner.on(":").join(widths));
             }
 
             return attrs;
         }
-    }
-
-    private Set<String> firstVertices(DirectedGraph<String, DefaultEdge> graph) {
-        Set<String> zeroInDegrees = new HashSet<String>();
-
-        for (String vertex : graph.vertexSet()) {
-            if (graph.inDegreeOf(vertex) == 0) {
-                zeroInDegrees.add(vertex);
-            }
-        }
-
-        return zeroInDegrees;
-    }
-
-    private DirectedGraph<String, DefaultEdge> simplify(DirectedGraph<String, DefaultEdge> graph, String startingVertex) {
-        DirectedGraph<String, DefaultEdge> sg1 = new DefaultDirectedGraph<String, DefaultEdge>(DefaultEdge.class);
-
-        StringBuilder contig = new StringBuilder(startingVertex.substring(0, startingVertex.length() - 1));
-
-        Set<String> seenVertices = new HashSet<String>();
-        String currentVertex = startingVertex;
-
-        while (graph.outDegreeOf(currentVertex) > 0 && !seenVertices.contains(currentVertex)) {
-            contig.append(currentVertex.charAt(currentVertex.length() - 1));
-
-            seenVertices.add(currentVertex);
-
-            Set<DefaultEdge> edges = graph.outgoingEdgesOf(currentVertex);
-
-            if (edges.size() == 1) {
-                DefaultEdge edge = edges.iterator().next();
-
-                String nextVertex = graph.getEdgeTarget(edge);
-
-                if (graph.inDegreeOf(nextVertex) == 1) {
-                    currentVertex = nextVertex;
-                } else {
-                    Graphs.addGraph(sg1, graph);
-                    sg1.removeAllVertices(seenVertices);
-                    sg1.addVertex(contig.toString());
-
-                    if (graph.inDegreeOf(startingVertex) > 0) {
-                        for (DefaultEdge inEdge : graph.incomingEdgesOf(startingVertex)) {
-                            sg1.addEdge(graph.getEdgeSource(inEdge), contig.toString());
-                        }
-                    }
-
-                    sg1.addEdge(contig.toString(), nextVertex);
-
-                    return simplify(sg1, nextVertex);
-                }
-            } else {
-                Graphs.addGraph(sg1, graph);
-                sg1.removeAllVertices(seenVertices);
-                sg1.addVertex(contig.toString());
-
-                if (graph.inDegreeOf(startingVertex) > 0) {
-                    for (DefaultEdge inEdge : graph.incomingEdgesOf(startingVertex)) {
-                        //if (sg1.containsVertex(graph.getEdgeSource(inEdge)) && sg1.containsVertex(contig.toString())) {
-                            sg1.addEdge(graph.getEdgeSource(inEdge), contig.toString());
-                        //}
-                    }
-                }
-
-                for (DefaultEdge edge : edges) {
-                    String nextVertex = graph.getEdgeTarget(edge);
-
-                    //if (sg1.containsVertex(contig.toString()) && sg1.containsVertex(nextVertex)) {
-                        sg1.addEdge(contig.toString(), nextVertex);
-                    //}
-
-                    sg1 = simplify(sg1, nextVertex);
-                }
-
-                return sg1;
-            }
-        }
-
-        seenVertices.add(currentVertex);
-
-        contig.append(currentVertex.charAt(currentVertex.length() - 1));
-
-        Graphs.addGraph(sg1, graph);
-        sg1.removeAllVertices(seenVertices);
-        sg1.addVertex(contig.toString());
-
-        if (graph.inDegreeOf(startingVertex) > 0) {
-            for (DefaultEdge inEdge : graph.incomingEdgesOf(startingVertex)) {
-                sg1.addEdge(graph.getEdgeSource(inEdge), contig.toString());
-            }
-        }
-
-        return sg1;
-    }
-
-    private DirectedGraph<String, DefaultEdge> simplify(DirectedGraph<String, DefaultEdge> graph) {
-        Set<String> firstVertices = firstVertices(graph);
-
-        DirectedGraph<String, DefaultEdge> simplifiedGraph = new DefaultDirectedGraph<String, DefaultEdge>(DefaultEdge.class);
-        Graphs.addGraph(simplifiedGraph, graph);
-
-        int count = 1;
-        for (String vertex : firstVertices) {
-            log.info("Simplifying graph ({}/{})", count, firstVertices.size());
-            simplifiedGraph = simplify(simplifiedGraph, vertex);
-
-            count++;
-        }
-
-        return simplifiedGraph;
     }
 
     public void writeGraph(DirectedGraph<String, DefaultEdge> g, CustomEdgeAttributeProvider eap, File f) {
@@ -218,17 +139,18 @@ public class VisualizeSequenceTree extends Module {
 
     @Override
     public void execute() {
+        // Load genes
         DirectedGraph<String, DefaultEdge> graph = new DefaultDirectedGraph<String, DefaultEdge>(DefaultEdge.class);
         Map<String, String> sequences = new HashMap<String, String>();
 
         ReferenceSequence rseq;
         while ((rseq = FASTA.nextSequence()) != null) {
+            // Load each gene into its own graph object.
             DirectedGraph<String, DefaultEdge> sampleGraph = new DefaultDirectedGraph<String, DefaultEdge>(DefaultEdge.class);
-
-            String seq = new String(rseq.getBases());
-
             Set<String> prevKmers = new HashSet<String>();
             String prevKmer = null;
+
+            String seq = new String(rseq.getBases());
 
             for (int i = 0; i <= seq.length() - KMER_SIZE; i++) {
                 String kmer = seq.substring(i, i + KMER_SIZE);
@@ -246,11 +168,13 @@ public class VisualizeSequenceTree extends Module {
                 }
             }
 
+            // Add all the graphs together.
             Graphs.addGraph(graph, sampleGraph);
 
             sequences.put(rseq.getName(), seq);
         }
 
+        // Find all of the branch points in the graph.
         Set<String> branchVertices = new HashSet<String>();
         for (String vertex : graph.vertexSet()) {
             if (graph.inDegreeOf(vertex) != 1 || graph.outDegreeOf(vertex) != 1) {
@@ -262,6 +186,8 @@ public class VisualizeSequenceTree extends Module {
         if (SKIP_SIMPLIFICATION) {
             finalGraph = graph;
         } else {
+            // Build the complete graph again, but this time, break the sequences into
+            // variable sized chunks based on where the branching kmers are found.
             finalGraph = new DefaultDirectedGraph<String, DefaultEdge>(DefaultEdge.class);
             for (String seqName : sequences.keySet()) {
                 String seq = sequences.get(seqName);
@@ -292,9 +218,19 @@ public class VisualizeSequenceTree extends Module {
             }
         }
 
-        // DirectedGraph<String, DefaultEdge> finalGraph = SKIP_SIMPLIFICATION ? graph : simplify(graph);
-
+        // Annotate the graph with some useful information.
         CustomEdgeAttributeProvider eap = new CustomEdgeAttributeProvider();
+
+        Map<String, String> colorMap = new HashMap<String, String>();
+        if (HIGHLIGHT != null) {
+            for (String regexColor : HIGHLIGHT) {
+                String[] pieces = regexColor.split(":");
+                String regex = pieces[0];
+                String color = pieces[1];
+
+                colorMap.put(regex, color);
+            }
+        }
 
         for (String seqName : sequences.keySet()) {
             String seq = sequences.get(seqName);
@@ -311,11 +247,33 @@ public class VisualizeSequenceTree extends Module {
                 String targetVertex = finalGraph.getEdgeTarget(edge);
 
                 if (vertices.contains(sourceVertex) && vertices.contains(targetVertex)) {
-                    eap.incrementWeight(edge);
+                    boolean found = false;
+                    for (String regex : colorMap.keySet()) {
+                        if (seqName.matches(regex)) {
+                            eap.addColor(edge, colorMap.get(regex));
+                            found = true;
+
+                            eap.incrementWeight(edge, colorMap.get(regex));
+
+                        }
+                    }
+
+                    if (!found) {
+                        eap.addColor(edge, "#000000");
+                        eap.incrementWeight(edge, "#000000");
+                    }
                 }
             }
         }
 
+        log.info("Sequences: {}", sequences.size());
+        log.info("Vertices: {}", graph.vertexSet().size());
+        log.info("Edges: {}", graph.edgeSet().size());
+        log.info("Simplified vertices: {}", finalGraph.vertexSet().size());
+        log.info("Simplified branch vertices: {}", branchVertices.size());
+        log.info("Simplified edges: {}", finalGraph.edgeSet().size());
+
+        // Write the graph to disk.
         writeGraph(finalGraph, eap, out);
     }
 }
