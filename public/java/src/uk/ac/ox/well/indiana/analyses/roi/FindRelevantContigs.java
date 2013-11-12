3411ce574ca82d04df830eecc3c2dca21a3cd628
Kiran V Garimella <kiran@well.ox.ac.uk>
2013-11-12 11:51:51 +0000

    Latest changes to the tree visualizer

---
+++ public/java/src/uk/ac/ox/well/indiana/analyses/roi/FindRelevantContigs.java
@@ -0,0 +1,18 @@
+package uk.ac.ox.well.indiana.analyses.roi;
+
+import net.sf.picard.reference.FastaSequenceFile;
+import uk.ac.ox.well.indiana.tools.Module;
+import uk.ac.ox.well.indiana.utils.arguments.Argument;
+
+public class FindRelevantContigs extends Module {
+    @Argument(fullName="sequences", shortName="s", doc="Sequences FASTA")
+    public FastaSequenceFile FASTA;
+
+    @Argument(fullName="kmerSize", shortName="ks", doc="Kmer size")
+    public Integer KMER_SIZE = 31;
+
+    @Override
+    public void execute() {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+}
