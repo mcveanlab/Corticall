d86e6c864a224b2de07b5debf303441d6bfaf5ba
Kiran V Garimella <kiran@well.ox.ac.uk>
2013-12-03 18:26:46 +0000

    Trying out some new normalization code

--- public/java/src/uk/ac/ox/well/indiana/analyses/roi/CoverageOfSpecificKmers.java
+++ public/java/src/uk/ac/ox/well/indiana/analyses/roi/CoverageOfSpecificKmers.java
@@ -19,8 +19,8 @@ public class CoverageOfSpecificKmers extends Module {
     @Argument(fullName="kmers", shortName="kmers", doc="Kmers to look for")
     public HashSet<String> KMERS;
 
-    @Argument(fullName="normalize", shortName="n", doc="Normalize?")
-    public Boolean NORMALIZE = false;
+    @Argument(fullName="normalizationGraph", shortName="n", doc="Normalize?")
+    public CortexGraph NORMALIZATION_GRAPH;
 
     @Output
     public PrintStream out;
@@ -28,26 +28,28 @@ public class CoverageOfSpecificKmers extends Module {
     @Override
     public void execute() {
         Map<String, Map<String, Integer>> results = new TreeMap<String, Map<String, Integer>>();
-        Map<String, Long> coverages = new HashMap<String, Long>();
+        Map<String, Double> norms = new HashMap<String, Double>();
+
+        // Find kmers that are unique in the genome
+        Set<CortexRecord> unitCoverageKmers = new HashSet<CortexRecord>();
+        for (CortexRecord cr : NORMALIZATION_GRAPH) {
+            int cov = cr.getCoverage(0);
+
+            if (cov == 1) {
+                unitCoverageKmers.add(cr);
+            }
+        }
 
         int numRecords = 0;
         int index = 0;
 
         for (CortexRecord cr : CORTEX_GRAPH) {
-            if (index % (CORTEX_GRAPH.getNumRecords() / 20) == 0) {
-                log.info("processed {} records", index);
-            }
-
             String fw = cr.getKmerAsString();
             String rc = SequenceUtils.reverseComplement(fw);
-
             String kmer = null;
 
-            if (KMERS.contains(fw)) {
-                kmer = fw;
-            } else if (KMERS.contains(rc)) {
-                kmer = rc;
-            }
+            if (KMERS.contains(fw)) { kmer = fw; }
+            else if (KMERS.contains(rc)) { kmer = rc; }
 
             if (kmer != null) {
                 if (!results.containsKey(kmer)) {
@@ -61,22 +63,31 @@ public class CoverageOfSpecificKmers extends Module {
                 }
             }
 
-            if (index % 1000 == 0) {
+            if (unitCoverageKmers.contains(cr.getKmer())) {
                 for (int color = 0; color < CORTEX_GRAPH.getNumColors(); color++) {
                     String sample = CORTEX_GRAPH.getColor(color).getSampleName();
                     int cov = cr.getCoverage(color);
 
-                    if (!coverages.containsKey(sample)) {
-                        coverages.put(sample, 0l);
+                    if (!norms.containsKey(sample)) {
+                        norms.put(sample, 0.0);
                     }
 
-                    coverages.put(sample, coverages.get(sample) + cov);
+                    norms.put(sample, norms.get(sample) + cov);
                 }
 
                 numRecords++;
             }
 
+            // Show our progress
+            if (index % (CORTEX_GRAPH.getNumRecords() / 10) == 0) {
+                log.info("processed {} records", index);
+            }
             index++;
+        }
+
+        for (String sample : norms.keySet()) {
+            double norm = norms.get(sample) / (double) numRecords;
+            norms.put(sample, norm);
         }
 
         Set<String> samples = results.get(results.keySet().iterator().next()).keySet();
@@ -86,11 +97,10 @@ public class CoverageOfSpecificKmers extends Module {
             List<String> fields = new ArrayList<String>();
 
             for (String sample : results.get(kmer).keySet()) {
-                float coverage = results.get(kmer).get(sample);
-                float average = (float) coverages.get(sample) / (float) numRecords;
-                float norm = (NORMALIZE) ? coverage / average : coverage;
+                double coverage = results.get(kmer).get(sample);
+                double normalizedCoverage = coverage / norms.get(sample);
 
-                fields.add(String.valueOf(coverage) + ":" + String.valueOf(norm) + ":" + String.valueOf(coverages.get(sample)) + ":" + String.valueOf(numRecords));
+                fields.add(String.valueOf(coverage) + ":" + String.valueOf(normalizedCoverage));
             }
 
             out.println(kmer + "\t" + Joiner.on("\t").join(fields));
