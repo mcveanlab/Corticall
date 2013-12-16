a7971db92012e36280f9ccb302db533bcd2afadb
Kiran V Garimella <kiran@well.ox.ac.uk>
2013-12-16 04:08:54 +0000

    Added a function to compute the longest common substring.  Added a method to compute the LCS between contigs from two different samples.  At the moment, only the contig pair that yields the largest overlap gets printed.

--- public/java/src/uk/ac/ox/well/indiana/utils/sequence/SequenceUtils.java
+++ public/java/src/uk/ac/ox/well/indiana/utils/sequence/SequenceUtils.java
@@ -452,4 +452,28 @@ public class SequenceUtils {
             throw new RuntimeException("Did not find corresponding amino acid for '" + codon + "'");
         }
     }
+
+    public static String longestCommonSubstring(String S1, String S2) {
+        // Taken from http://en.wikibooks.org/wiki/Algorithm_Implementation/Strings/Longest_common_substring#Java
+
+        int Start = 0;
+        int Max = 0;
+
+        for (int i = 0; i < S1.length(); i++) {
+            for (int j = 0; j < S2.length(); j++) {
+                int x = 0;
+                while (S1.charAt(i + x) == S2.charAt(j + x)) {
+                    x++;
+                    if (((i + x) >= S1.length()) || ((j + x) >= S2.length())) break;
+                }
+
+                if (x > Max) {
+                    Max = x;
+                    Start = i;
+                }
+            }
+        }
+
+        return S1.substring(Start, (Start + Max));
+    }
 }
