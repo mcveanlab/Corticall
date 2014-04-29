500a494be0b68aebbbe0d4b1bebadda06fbc99b5
Kiran V Garimella <kiran@well.ox.ac.uk>
2014-04-29 10:11:26 +0100

    Outputs contigs that span recombination events (based on alignments to the canonical reference
    Former-commit-id: c5c5a0dd25b23425adfff5ce82deaf103c003d3f

---
+++ public/java/src/uk/ac/ox/well/indiana/attic/analyses/nahr/FindContigsThatSpanRecombinationEvents.java
@@ -0,0 +1,93 @@
+package uk.ac.ox.well.indiana.attic.analyses.nahr;
+
+import net.sf.picard.util.Interval;
+import net.sf.samtools.CigarElement;
+import net.sf.samtools.CigarOperator;
+import net.sf.samtools.SAMFileReader;
+import net.sf.samtools.SAMRecord;
+import uk.ac.ox.well.indiana.commands.Module;
+import uk.ac.ox.well.indiana.utils.arguments.Argument;
+import uk.ac.ox.well.indiana.utils.arguments.Output;
+import uk.ac.ox.well.indiana.utils.io.table.TableReader;
+import uk.ac.ox.well.indiana.utils.io.table.TableWriter;
+
+import java.io.File;
+import java.io.PrintStream;
+import java.util.*;
+
+public class FindContigsThatSpanRecombinationEvents extends Module {
+    @Argument(fullName="bam", shortName="b", doc="BAM file")
+    public ArrayList<SAMFileReader> BAMS;
+
+    @Argument(fullName="recombLoci", shortName="rl", doc="Recomb loci table")
+    public File RECOMB_LOCI;
+
+    @Output
+    public PrintStream out;
+
+    @Override
+    public void execute() {
+        Map<String, Set<Interval>> loci = new HashMap<String, Set<Interval>>();
+
+        TableReader tr = new TableReader(RECOMB_LOCI);
+
+        for (Map<String, String> te : tr) {
+            String[] sampleFields = te.get("sample").split("/");
+            String sampleName = sampleFields[1];
+
+            Interval locus = new Interval(te.get("chrom"), Integer.valueOf(te.get("pos_max")), Integer.valueOf(te.get("pos_max")));
+
+            if (!loci.containsKey(sampleName)) {
+                loci.put(sampleName, new HashSet<Interval>());
+            }
+
+            loci.get(sampleName).add(locus);
+        }
+
+        TableWriter tw = new TableWriter(out);
+
+        for (SAMFileReader BAM : BAMS) {
+            String sampleName = BAM.getFileHeader().getReadGroups().iterator().next().getSample();
+
+            for (SAMRecord contig : BAM) {
+                Interval contigLocus = new Interval(contig.getReferenceName(), contig.getAlignmentStart(), contig.getAlignmentEnd());
+
+                for (Interval knownLocus : loci.get(sampleName)) {
+                    if (knownLocus.intersects(contigLocus)) {
+                        boolean isClipped = false;
+                        for (CigarElement ce : contig.getCigar().getCigarElements()) {
+                            if (ce.getOperator().equals(CigarOperator.S) || ce.getOperator().equals(CigarOperator.H)) {
+                                isClipped = true;
+                                break;
+                            }
+                        }
+
+                        boolean perfectAlignment = false;
+                        if (contig.getCigar().getCigarElements().size() == 1 && contig.getCigar().getCigarElement(0).getOperator().equals(CigarOperator.M)) {
+                            String md = contig.getStringAttribute("MD");
+                            try {
+                                int length = Integer.parseInt(md);
+                                if (length == contig.getReadLength()) {
+                                    perfectAlignment = true;
+                                }
+                            } catch (NumberFormatException e) {}
+                        }
+
+                        Map<String, String> te = new LinkedHashMap<String, String>();
+                        te.put("sampleName", sampleName);
+                        te.put("contigName", contig.getReadName());
+                        te.put("contigLocus", contigLocus.toString());
+                        te.put("knownLocus", knownLocus.toString());
+                        te.put("contigLength", String.valueOf(contig.getReadLength()));
+                        te.put("isClipped", isClipped ? "1" : "0");
+                        te.put("perfectAlignment", perfectAlignment ? "1" : "0");
+                        te.put("cigar", contig.getCigarString());
+                        te.put("md", contig.getStringAttribute("MD"));
+
+                        tw.addEntry(te);
+                    }
+                }
+            }
+        }
+    }
+}
