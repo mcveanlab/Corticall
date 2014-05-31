aae8393b3b1278f45bf5d92c1c3c6bc79ba40479
Kiran V Garimella <kiran@well.ox.ac.uk>
2014-05-31 22:38:43 +0100

    Modules to examine errors in assembled contigs based on their alignment back to a reference sequence
    Former-commit-id: 5f47b8b4a9e3c5e01b03605abc8e743ef5007500

---
+++ public/java/src/uk/ac/ox/well/indiana/attic/analyses/ContigErrors/BasicAssemblyStats.java
@@ -0,0 +1,59 @@
+package uk.ac.ox.well.indiana.attic.analyses.ContigErrors;
+
+import net.sf.picard.reference.FastaSequenceFile;
+import net.sf.picard.reference.ReferenceSequence;
+import uk.ac.ox.well.indiana.commands.Module;
+import uk.ac.ox.well.indiana.utils.arguments.Argument;
+import uk.ac.ox.well.indiana.utils.arguments.Output;
+import uk.ac.ox.well.indiana.utils.io.table.TableWriter;
+import uk.ac.ox.well.indiana.utils.sequence.SequenceUtils;
+
+import java.io.PrintStream;
+import java.util.*;
+
+public class BasicAssemblyStats extends Module {
+    @Argument(fullName="contigs", shortName="c", doc="Contigs (FASTA)")
+    public TreeMap<String, FastaSequenceFile> CONTIGS;
+
+    @Argument(fullName="lengthMin", shortName="lmin", doc="Minimum contig length")
+    public Integer LENGTH_MIN = 0;
+
+    @Output
+    public PrintStream out;
+
+    @Override
+    public void execute() {
+        TableWriter tw = new TableWriter(out);
+
+        for (String id : CONTIGS.keySet()) {
+            Map<String, String> entry = new LinkedHashMap<String, String>();
+
+            List<String> seqs = new ArrayList<String>();
+
+            FastaSequenceFile contigs = CONTIGS.get(id);
+            ReferenceSequence rseq;
+            while ((rseq = contigs.nextSequence()) != null) {
+                String seq = new String(rseq.getBases());
+
+                if (seq.length() >= LENGTH_MIN) {
+                    seqs.add(seq);
+                }
+            }
+
+            int numContigs = seqs.size();
+            int minLength = SequenceUtils.minLength(seqs);
+            int maxLength = SequenceUtils.maxLength(seqs);
+            float meanLength = SequenceUtils.meanLength(seqs);
+            int n50 = SequenceUtils.computeN50Value(seqs);
+
+            entry.put("id", id);
+            entry.put("numContigs", String.valueOf(numContigs));
+            entry.put("minLength", String.valueOf(minLength));
+            entry.put("maxLength", String.valueOf(maxLength));
+            entry.put("meanLength", String.format("%.2f", meanLength));
+            entry.put("n50", String.valueOf(n50));
+
+            tw.addEntry(entry);
+        }
+    }
+}
