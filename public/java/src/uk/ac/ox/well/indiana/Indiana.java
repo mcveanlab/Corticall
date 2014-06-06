4d9ed4c719d0b33dc633ab278866701096a08ea9
Kiran V Garimella <kiran@well.ox.ac.uk>
2014-06-06 14:05:58 +0100

    Commented out the line that tells the user exactly what the command invocation was (including default arguments not specified on the command line) because of some bugs in producing that string that I haven't fixed yet.

--- public/java/src/uk/ac/ox/well/indiana/Indiana.java
+++ public/java/src/uk/ac/ox/well/indiana/Indiana.java
@@ -4,7 +4,7 @@ public class Indiana {
     public static final String progName = "INDIANA";
     public static final String progDesc = "tools for constructing and manipulating de-novo assembly data";
     public static final String rootPackage = "uk.ac.ox.well.indiana";
-    public static final String commandPackage = "uk.ac.ox.well.indiana.commands";
+    public static final String commandPackage = "uk.ac.ox.well.indiana.commands.visualization";
 
     public static void main(String[] args) throws Exception {
         Main.start(progName, progDesc, rootPackage, commandPackage, args);
