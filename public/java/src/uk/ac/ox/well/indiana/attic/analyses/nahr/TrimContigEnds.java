d4fc8871cde155922a70cc54ebdccb86a9e283f2
Kiran V Garimella <kiran@well.ox.ac.uk>
2014-06-07 12:17:01 +0100

    Utility to trim back contigs
    Former-commit-id: 6d713c4dd8845b7ef91e14eb10e6e29fd3e9a426

---
+++ public/java/src/uk/ac/ox/well/indiana/attic/analyses/nahr/TrimContigEnds.java
@@ -0,0 +1,34 @@
+package uk.ac.ox.well.indiana.attic.analyses.nahr;
+
+import net.sf.picard.reference.FastaSequenceFile;
+import net.sf.picard.reference.ReferenceSequence;
+import uk.ac.ox.well.indiana.commands.Module;
+import uk.ac.ox.well.indiana.utils.arguments.Argument;
+import uk.ac.ox.well.indiana.utils.arguments.Output;
+
+import java.io.PrintStream;
+
+public class TrimContigEnds extends Module {
+    @Argument(fullName="contigs", shortName="c", doc="Contigs (FASTA)")
+    public FastaSequenceFile CONTIGS;
+
+    @Argument(fullName="trim", shortName="t", doc="Number of bases to trim from both the left and right sides of the contig")
+    public Integer TRIM = 5;
+
+    @Output
+    public PrintStream out;
+
+    @Override
+    public void execute() {
+        ReferenceSequence rseq;
+
+        while ((rseq = CONTIGS.nextSequence()) != null) {
+            String seq = new String(rseq.getBases());
+
+            String subseq = seq.substring(TRIM, seq.length() - TRIM);
+
+            out.println(">" + rseq.getName());
+            out.println(subseq);
+        }
+    }
+}
