72607fa53d9159a7a878f68ca23960e7f9613405
Kiran V Garimella <kiran@well.ox.ac.uk>
2014-06-23 11:28:43 +0100

    Export masked kmers as a FASTA file
    Former-commit-id: 2d3d5d132196e636444e880c16346f0454c5f56d

---
+++ public/java/src/uk/ac/ox/well/indiana/attic/analyses/nahr/GetAmbiguousKmers.java
@@ -0,0 +1,74 @@
+package uk.ac.ox.well.indiana.attic.analyses.nahr;
+
+import net.sf.picard.reference.FastaSequenceFile;
+import net.sf.picard.reference.IndexedFastaSequenceFile;
+import net.sf.picard.reference.ReferenceSequence;
+import uk.ac.ox.well.indiana.commands.Module;
+import uk.ac.ox.well.indiana.utils.arguments.Argument;
+import uk.ac.ox.well.indiana.utils.arguments.Output;
+import uk.ac.ox.well.indiana.utils.io.cortex.CortexKmer;
+import uk.ac.ox.well.indiana.utils.io.utils.LineReader;
+
+import java.io.File;
+import java.io.PrintStream;
+import java.util.HashMap;
+import java.util.Map;
+
+public class GetAmbiguousKmers extends Module {
+    @Argument(fullName="fasta", shortName="f", doc="FASTA")
+    public IndexedFastaSequenceFile FASTA;
+
+    @Argument(fullName="mask", shortName="m", doc="Mask")
+    public File MASK;
+
+    @Argument(fullName="kmerSize", shortName="ks", doc="Kmer size")
+    public Integer KMER_SIZE = 47;
+
+    @Output
+    public PrintStream out;
+
+    @Output(fullName="statsOut", shortName="so", doc="Stats out")
+    public PrintStream sout;
+
+    @Override
+    public void execute() {
+        Map<String, Integer> kmerCategories = new HashMap<String, Integer>();
+
+        LineReader lr = new LineReader(MASK);
+        String line;
+        int lineNum = 0;
+        while ((line = lr.getNextRecord()) != null) {
+            if (lineNum >= 3) {
+                String[] fields = line.split("\\s+");
+                String chr = fields[5];
+                int start = Integer.valueOf(fields[6]);
+                int stop = Integer.valueOf(fields[7]);
+                String type = fields[11];
+
+                log.info("chr={} start={} end={} type={}", chr, start, stop, type);
+
+                ReferenceSequence rseq = FASTA.getSubsequenceAt(chr, start, stop);
+                String seq = new String(rseq.getBases());
+
+                for (int i = 0; i <= seq.length() - KMER_SIZE; i++) {
+                    CortexKmer kmer = new CortexKmer(seq.substring(i, i + KMER_SIZE));
+
+                    String name = String.format("%s:%d-%d.%d %s", chr, start, stop, i, type);
+                    out.println(">" + name);
+                    out.println(kmer.getKmerAsString());
+
+                    if (!kmerCategories.containsKey(type)) {
+                        kmerCategories.put(type, 0);
+                    }
+                    kmerCategories.put(type, kmerCategories.get(type) + 1);
+                }
+            }
+
+            lineNum++;
+        }
+
+        for (String key : kmerCategories.keySet()) {
+            sout.printf("%s\t%d\n", key, kmerCategories.get(key));
+        }
+    }
+}
