c15a3507e3ecc2833e1c965c34a7be80bddb0153
Kiran V Garimella <kiran@well.ox.ac.uk>
2014-06-30 17:14:48 +0100

    Output coverage distributions for parental kmers
    Former-commit-id: 64be1d1965bc0f05f4574c42c040382ca96562de

---
+++ public/java/src/uk/ac/ox/well/indiana/attic/analyses/nahr/GetParentKmerCoverage.java
@@ -0,0 +1,66 @@
+package uk.ac.ox.well.indiana.attic.analyses.nahr;
+
+import com.google.common.base.Joiner;
+import uk.ac.ox.well.indiana.commands.Module;
+import uk.ac.ox.well.indiana.utils.arguments.Argument;
+import uk.ac.ox.well.indiana.utils.arguments.Output;
+import uk.ac.ox.well.indiana.utils.io.cortex.CortexGraph;
+import uk.ac.ox.well.indiana.utils.io.cortex.CortexRecord;
+
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.List;
+
+public class GetParentKmerCoverage extends Module {
+    @Argument(fullName="parents", shortName="p", doc="Parents (in Cortex format)")
+    public CortexGraph PARENTS;
+
+    @Output(fullName="oref0", shortName="o0", doc="Ref0 coverage")
+    public PrintStream oref0;
+
+    @Output(fullName="oref1", shortName="o1", doc="Ref1 coverage")
+    public PrintStream oref1;
+
+    @Override
+    public void execute() {
+        int ref0 = 0;
+        int ref1 = 0;
+        int refBoth = 0;
+        int refTotal = 0;
+
+        /*
+        List<Integer> coverage0 = new ArrayList<Integer>();
+        List<Integer> coverage1 = new ArrayList<Integer>();
+        List<Integer> coverageBoth = new ArrayList<Integer>();
+        */
+
+        log.info("Computing coverage distribution for diagnostic kmers...");
+
+        int numRecords = 0;
+        for (CortexRecord cr : PARENTS) {
+            if (numRecords % (PARENTS.getNumRecords() / 5) == 0) {
+                log.info("  processed {}/{} (~{}%) records", numRecords, PARENTS.getNumRecords(), String.format("%.2f", 100.0f*((float) numRecords / (float) PARENTS.getNumRecords())));
+            }
+            numRecords++;
+
+            if (cr.getCoverage(0) == 0 && cr.getCoverage(1) > 0) {
+                // HB3
+                ref1++;
+                oref1.println(cr.getCoverage(1));
+            } else if (cr.getCoverage(0) > 0 && cr.getCoverage(1) == 0) {
+                // 3D7
+                ref0++;
+                oref0.println(cr.getCoverage(0));
+            } else {
+                refBoth++;
+            }
+
+            refTotal++;
+        }
+
+        log.info("Number of diagnostic kmers:");
+        log.info("  ref0: {}/{} (~{}%)", ref0, refTotal, String.format("%.2f", 100.0f*((float) ref0)/((float) refTotal)));
+        log.info("  ref1: {}/{} (~{}%)", ref1, refTotal, String.format("%.2f", 100.0f*((float) ref1)/((float) refTotal)));
+        log.info("  refBoth: {}/{} (~{}%)", refBoth, refTotal, String.format("%.2f", 100.0f*((float) refBoth)/((float) refTotal)));
+    }
+}
