842a79c8072845a671100e373765e8eb46b0f84b
Kiran V Garimella <kiran@well.ox.ac.uk>
2014-07-15 17:13:13 +0100

    Output a table where the genotype is replaced with a code that represents the parent that transmitted the genotype
    Former-commit-id: e22cea548ec95f4c958fa5e39a166f989bc0831d

--- public/java/src/uk/ac/ox/well/indiana/attic/analyses/nahr/PaintTransmittedHaplotypes.java
+++ public/java/src/uk/ac/ox/well/indiana/attic/analyses/nahr/PaintTransmittedHaplotypes.java
@@ -1,7 +1,77 @@
 package uk.ac.ox.well.indiana.attic.analyses.nahr;
 
-/**
- * Created by kiran on 15/07/2014.
- */
-public class PaintTransmittedHaplotypes {
+import htsjdk.variant.variantcontext.Genotype;
+import htsjdk.variant.variantcontext.VariantContext;
+import htsjdk.variant.vcf.VCFFileReader;
+import uk.ac.ox.well.indiana.commands.Module;
+import uk.ac.ox.well.indiana.utils.arguments.Argument;
+import uk.ac.ox.well.indiana.utils.arguments.Output;
+import uk.ac.ox.well.indiana.utils.io.table.TableWriter;
+
+import java.io.PrintStream;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class PaintTransmittedHaplotypes extends Module {
+    @Argument(fullName="vcf", shortName="v", doc="VCF")
+    public VCFFileReader VCF;
+
+    @Argument(fullName="parent1", shortName="p1", doc="Parent 1 sample name")
+    public String PARENT1;
+
+    @Argument(fullName="parent2", shortName="p2", doc="Parent 2 sample name")
+    public String PARENT2;
+
+    @Output
+    public PrintStream out;
+
+    @Override
+    public void execute() {
+        TableWriter tw = new TableWriter(out);
+
+        log.info("Processing variants...");
+        int numVariants = 0;
+        for (VariantContext vc : VCF) {
+            if (numVariants % 10000 == 0) {
+                log.info("  {}", numVariants);
+            }
+            numVariants++;
+
+            if (!vc.isFiltered()) {
+                Genotype g1 = vc.getGenotype(PARENT1);
+                Genotype g2 = vc.getGenotype(PARENT2);
+
+                Map<String, String> te = new LinkedHashMap<String, String>();
+
+                te.put("CHROM", vc.getChr());
+                te.put("POS", String.valueOf(vc.getStart()));
+
+                for (String sampleName : vc.getSampleNames()) {
+                    if (sampleName.equals(PARENT1)) {
+                        te.put(PARENT1, "1");
+                    } else if (sampleName.equals(PARENT2)) {
+                        te.put(PARENT2, "2");
+                    } else {
+                        Genotype gp = vc.getGenotype(sampleName);
+
+                        int parentIndex = 0;
+
+                        if ( ((gp.getPloidy() == 1) || (g1.isPhased() && g2.isPhased() && gp.isPhased())) &&
+                             (!g1.isNoCall() && !g2.isNoCall() && !gp.isNoCall()) ) {
+
+                            if (gp.sameGenotype(g1, false) && !gp.sameGenotype(g2, false)) {
+                                parentIndex = 1;
+                            } else if (gp.sameGenotype(g2, false) && !gp.sameGenotype(g1, false)) {
+                                parentIndex = 2;
+                            }
+                        }
+
+                        te.put(sampleName, String.valueOf(parentIndex));
+                    }
+                }
+
+                tw.addEntry(te);
+            }
+        }
+    }
 }
