0e84912115567679c53668f2dc5259828b970803
Kiran V Garimella <kiran@well.ox.ac.uk>
2014-08-28 18:09:32 +0100

    Common tasks when manipulating Cortex records
    Former-commit-id: 843fac21b048f6514e14adc983761fd3926ffa7d

---
+++ public/java/src/uk/ac/ox/well/indiana/utils/sequence/CortexUtils.java
@@ -0,0 +1,132 @@
+package uk.ac.ox.well.indiana.utils.sequence;
+
+import uk.ac.ox.well.indiana.utils.io.cortex.graph.CortexGraph;
+import uk.ac.ox.well.indiana.utils.io.cortex.graph.CortexKmer;
+import uk.ac.ox.well.indiana.utils.io.cortex.graph.CortexRecord;
+import uk.ac.ox.well.indiana.utils.io.cortex.paths.CortexJunctionsRecord;
+import uk.ac.ox.well.indiana.utils.io.cortex.paths.CortexPathsRecord;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * A set of utilities for dealing with Cortex graphs and records
+ */
+public class CortexUtils {
+    /**
+     * Private constructor - this class cannot be instantiated!
+     */
+    private CortexUtils() {}
+
+    /**
+     * Return the next kmer, in the orientation of the given kmer
+     *
+     * @param cg    the sorted Cortex graph
+     * @param kmer  the kmer
+     * @return      the next kmer (in the orientation of the given kmer)
+     */
+    public static String getNextKmer(CortexGraph cg, String kmer) {
+        CortexKmer ck = new CortexKmer(kmer);
+        CortexRecord cr = cg.findRecord(ck);
+
+        if (cr != null) {
+            Collection<String> outEdges = cr.getOutEdgesAsStrings(0);
+
+            if (ck.isFlipped()) {
+                outEdges = cr.getInEdgesComplementAsStrings(0);
+            }
+
+            if (outEdges.size() == 1) {
+                String outEdge = outEdges.iterator().next();
+
+                return kmer.substring(1, kmer.length()) + outEdge;
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * Return the next kmers, in the orientation of the given kmer
+     *
+     * @param cg    the sorted Cortex graph
+     * @param kmer  the kmer
+     * @return      the next kmers (in the orientation of the given kmer)
+     */
+    public static Set<String> getNextKmers(CortexGraph cg, String kmer) {
+        CortexKmer ck = new CortexKmer(kmer);
+        CortexRecord cr = cg.findRecord(ck);
+        Set<String> nextKmers = new HashSet<String>();
+
+        if (cr != null) {
+            Collection<String> outEdges = cr.getOutEdgesAsStrings(0);
+
+            if (ck.isFlipped()) {
+                outEdges = cr.getInEdgesComplementAsStrings(0);
+            }
+
+
+            for (String outEdge : outEdges) {
+                nextKmers.add(kmer.substring(1, kmer.length()) + outEdge);
+            }
+        }
+
+        return nextKmers;
+    }
+
+    /**
+     * Return the previous kmer, in the orientation of the given kmer
+     *
+     * @param cg    the sorted Cortex graph
+     * @param kmer  the kmer
+     * @return      the preivous kmer (in the orientation of the given kmer)
+     */
+    public static String getPrevKmer(CortexGraph cg, String kmer) {
+        CortexKmer ck = new CortexKmer(kmer);
+        CortexRecord cr = cg.findRecord(ck);
+
+        if (cr != null) {
+            Collection<String> inEdges = cr.getInEdgesAsStrings(0);
+
+            if (ck.isFlipped()) {
+                inEdges = cr.getOutEdgesComplementAsStrings(0);
+            }
+
+            if (inEdges.size() == 1) {
+                String inEdge = inEdges.iterator().next();
+
+                return inEdge + kmer.substring(0, kmer.length() - 1);
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * Return the previous kmers, in the orientation of the given kmer
+     *
+     * @param cg    the sorted Cortex graph
+     * @param kmer  the kmer
+     * @return      the preivous kmers (in the orientation of the given kmer)
+     */
+    public static Set<String> getPrevKmers(CortexGraph cg, String kmer) {
+        CortexKmer ck = new CortexKmer(kmer);
+        CortexRecord cr = cg.findRecord(ck);
+        Set<String> prevKmers = new HashSet<String>();
+
+        if (cr != null) {
+            Collection<String> inEdges = cr.getInEdgesAsStrings(0);
+
+            if (ck.isFlipped()) {
+                inEdges = cr.getOutEdgesComplementAsStrings(0);
+            }
+
+            for (String inEdge : inEdges) {
+                prevKmers.add(inEdge + kmer.substring(0, kmer.length() - 1));
+            }
+        }
+
+        return prevKmers;
+    }
+}
