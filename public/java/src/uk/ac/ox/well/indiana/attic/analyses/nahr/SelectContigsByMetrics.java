5a8d1c7d2e13554ada0fdaf7cfa0a9eb88e50b12
Kiran V Garimella <kiran@well.ox.ac.uk>
2014-09-04 15:49:13 +0100

    Added the classifications for each contig if we have a dataset that tells us what the aligned position of the contig corresponds to
    Former-commit-id: c7ddbf1df666da90b7d530c74624ce5cdc6d0302

--- public/java/src/uk/ac/ox/well/indiana/attic/analyses/nahr/SelectContigsByMetrics.java
+++ public/java/src/uk/ac/ox/well/indiana/attic/analyses/nahr/SelectContigsByMetrics.java
@@ -3,6 +3,8 @@ package uk.ac.ox.well.indiana.attic.analyses.nahr;
 import com.google.common.base.Joiner;
 import htsjdk.samtools.SAMFileReader;
 import htsjdk.samtools.SAMRecord;
+import htsjdk.samtools.util.Interval;
+import htsjdk.samtools.util.IntervalTreeMap;
 import org.apache.commons.jexl2.Expression;
 import org.apache.commons.jexl2.JexlContext;
 import org.apache.commons.jexl2.MapContext;
@@ -15,10 +17,7 @@ import uk.ac.ox.well.indiana.utils.io.table.TableWriter;
 
 import java.io.File;
 import java.io.PrintStream;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class SelectContigsByMetrics extends Module {
     @Argument(fullName="contigMetrics", shortName="cm", doc="Contig metrics")
@@ -27,11 +26,17 @@ public class SelectContigsByMetrics extends Module {
     @Argument(fullName="constraint", shortName="c", doc="A JEXL constraint to apply when selecting contigs to consider")
     public ArrayList<Expression> CONSTRAINTS;
 
-    @Argument(fullName="trainingContigs", shortName="tc", doc="Training contigs (BAM)")
-    public SAMFileReader TRAINING;
+    @Argument(fullName="alignedContigs", shortName="tc", doc="Aligned contigs (BAM)")
+    public SAMFileReader CONTIGS;
 
     @Argument(fullName="sampleName", shortName="sn", doc="Sample name")
     public String SAMPLE_NAME;
+
+    @Argument(fullName="accession", shortName="acc", doc="Accession")
+    public String ACCESSION;
+
+    @Argument(fullName="classifications", shortName="cl", doc="Classification files")
+    public LinkedHashMap<String, File> CLASSIFICATIONS;
 
     @Output
     public PrintStream out;
@@ -44,9 +49,45 @@ public class SelectContigsByMetrics extends Module {
 
     @Override
     public void execute() {
+        /*
         Set<String> knownEvents = new HashSet<String>();
         for (SAMRecord read : TRAINING) {
             knownEvents.add(read.getReadName());
+        }
+        */
+
+        Map<String, Interval> contigLoci = new HashMap<String, Interval>();
+        for (SAMRecord contig : CONTIGS) {
+            Interval it = new Interval(contig.getReferenceName(), contig.getAlignmentStart(), contig.getAlignmentEnd());
+
+            contigLoci.put(contig.getReadName(), it);
+        }
+
+        Map<String, IntervalTreeMap<String>> its = new HashMap<String, IntervalTreeMap<String>>();
+
+        int knownEvents = 0;
+        for (String type : CLASSIFICATIONS.keySet()) {
+            TableReader ctr = new TableReader(CLASSIFICATIONS.get(type));
+
+            for (Map<String, String> cte : ctr) {
+                String[] samplePieces = cte.get("sample").split("/");
+                String acc = samplePieces[2];
+                String chrom = cte.get("chrom");
+                Integer start = Integer.valueOf(cte.get("co_pos_min"));
+                Integer stop = Integer.valueOf(cte.get("co_pos_max"));
+
+                Interval it = new Interval(chrom, start, stop);
+
+                if (!its.containsKey(acc)) {
+                    its.put(acc, new IntervalTreeMap<String>());
+                }
+
+                its.get(acc).put(it, type);
+
+                if (ACCESSION.equals(acc)) {
+                    knownEvents++;
+                }
+            }
         }
 
         TableReader tr = new TableReader(ANN);
@@ -79,6 +120,7 @@ public class SelectContigsByMetrics extends Module {
             }
 
             if (allSatisfied) {
+                /*
                 te.put("isKnownAHR", "0");
 
                 if (knownEvents.contains(te.get("contigName"))) {
@@ -87,6 +129,22 @@ public class SelectContigsByMetrics extends Module {
                     te.put("isKnownAHR", "1");
                 }
                 selectedContigs++;
+                */
+
+                for (String type : CLASSIFICATIONS.keySet()) {
+                    te.put(type, "0");
+                }
+
+                Interval it = contigLoci.get(te.get("contigName"));
+
+                if (its.get(ACCESSION).containsOverlapping(it)) {
+                    Set<String> types = new TreeSet<String>();
+                    types.addAll(its.get(ACCESSION).getOverlapping(it));
+
+                    for (String type : types) {
+                        te.put(type, "1");
+                    }
+                }
 
                 tw.addEntry(te);
 
@@ -98,6 +156,6 @@ public class SelectContigsByMetrics extends Module {
 
         log.info("Found {}/{} (~{}%) contigs that met criteria, {} training contigs recovered.", selectedContigs, numContigs, String.format("%.2f", 100.0f * (float) selectedContigs / (float) numContigs), knownContigs);
 
-        sout.println(Joiner.on("\t").join(SAMPLE_NAME, numContigs, selectedContigs, knownEvents.size(), knownContigs));
+        sout.println(Joiner.on("\t").join(SAMPLE_NAME, numContigs, selectedContigs, knownEvents, knownContigs));
     }
 }
