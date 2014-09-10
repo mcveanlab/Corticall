edc123520888505580b9493426d50187f29ffd44
Kiran V Garimella <kiran@well.ox.ac.uk>
2014-09-10 13:15:42 +0100

    End of second link should be specified with radius2, not radius1

--- public/java/src/uk/ac/ox/well/indiana/attic/analyses/nahr/ComputeParentalContributionTracks.java
+++ public/java/src/uk/ac/ox/well/indiana/attic/analyses/nahr/ComputeParentalContributionTracks.java
@@ -122,7 +122,7 @@ public class ComputeParentalContributionTracks extends Module {
                 String[] ref1ToCanonicalExact = (te.get("ref1ToCanonicalExact").equals("NA") ? "NA:0-0" : te.get("ref1ToCanonicalExact")).split("[:-]");
 
                 cout.println(contigName + " " + ref0ToCanonicalExact[0] + " " + ref0ToCanonicalExact[1] + " " + ref0ToCanonicalExact[2] + " radius1=0.8r");
-                cout.println(contigName + " " + ref1ToCanonicalExact[0] + " " + ref1ToCanonicalExact[1] + " " + ref1ToCanonicalExact[2] + " radius1=0.6r");
+                cout.println(contigName + " " + ref1ToCanonicalExact[0] + " " + ref1ToCanonicalExact[1] + " " + ref1ToCanonicalExact[2] + " radius2=0.6r");
             }
         }
 
