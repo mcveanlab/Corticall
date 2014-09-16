b30ed9f98d88df96ed2a70cf3d95b0c8d243cd3d
Kiran V Garimella <kiran@well.ox.ac.uk>
2014-09-16 07:39:04 +0100

    Make sure contigs in the Circos plots are differentiated by the sample name and accession number, so as to avoid collisions when combining plots
    Former-commit-id: 0922a638d1b95a1bc2b3d801968dac041783d80b

--- public/java/src/uk/ac/ox/well/indiana/attic/analyses/nahr/ComputeParentalContributionTracks.java
+++ public/java/src/uk/ac/ox/well/indiana/attic/analyses/nahr/ComputeParentalContributionTracks.java
@@ -121,8 +121,8 @@ public class ComputeParentalContributionTracks extends Module {
                 String[] ref0ToCanonicalExact = (te.get("ref0ToCanonicalExact").equals("NA") || te.get("ref0ToCanonicalExact").equals("*:0-0") ? "NA:0-0" : te.get("ref0ToCanonicalExact")).split("[:-]");
                 String[] ref1ToCanonicalExact = (te.get("ref1ToCanonicalExact").equals("NA") || te.get("ref1ToCanonicalExact").equals("*:0-0") ? "NA:0-0" : te.get("ref1ToCanonicalExact")).split("[:-]");
 
-                cout.println(contigName + " " + ref0ToCanonicalExact[0] + " " + ref0ToCanonicalExact[1] + " " + ref0ToCanonicalExact[2] + " radius1=0.8r");
-                cout.println(contigName + " " + ref1ToCanonicalExact[0] + " " + ref1ToCanonicalExact[1] + " " + ref1ToCanonicalExact[2] + " radius2=0.6r");
+                cout.println(te.get("sampleName") + "_" + te.get("accession") + "_" + contigName + " " + ref0ToCanonicalExact[0] + " " + ref0ToCanonicalExact[1] + " " + ref0ToCanonicalExact[2] + " radius1=0.8r");
+                cout.println(te.get("sampleName") + "_" + te.get("accession") + "_" + contigName + " " + ref1ToCanonicalExact[0] + " " + ref1ToCanonicalExact[1] + " " + ref1ToCanonicalExact[2] + " radius2=0.6r");
             }
         }
 
