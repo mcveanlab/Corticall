48e7e798252f9ad09b59167b418f3bc83a31b4b5
Kiran V Garimella <kiran@well.ox.ac.uk>
2015-01-20 14:12:07 +0000

    Recombine 3D7 var genes that are present in the child.  Ensure that the recombination partners are of the same UPS class, are on different chromosomes, are telomeric, on the same side of the chromosome, and that the recombination breakpoints land in homologous regions.  Remove the old var genes and insert the recombined var gene in the first one's place.
    Former-commit-id: fc4003b504f82bdc830b62ed3ccea9bbe5fe4a0f

---
+++ public/java/src/uk/ac/ox/well/indiana/attic/analyses/nahr/ExtractRefSequenceForContig.java
@@ -0,0 +1,7 @@
+package uk.ac.ox.well.indiana.attic.analyses.nahr;
+
+/**
+ * Created by kiran on 05/01/2015.
+ */
+public class ExtractRefSequenceForContig {
+}
---
+++ public/java/src/uk/ac/ox/well/indiana/attic/analyses/nahr/KmersMissingInContigs.java
@@ -0,0 +1,7 @@
+package uk.ac.ox.well.indiana.attic.analyses.nahr;
+
+/**
+ * Created by kiran on 15/12/2014.
+ */
+public class KmersMissingInContigs {
+}
