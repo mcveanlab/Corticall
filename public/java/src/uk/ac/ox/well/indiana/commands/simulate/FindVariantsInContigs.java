6cfd7bd378b737356ccecb7c1ce62a93895a1cd7
Kiran V Garimella <kiran@well.ox.ac.uk>
2015-02-08 19:33:10 +0000

    Tool to find the de novo variants in the contigs
    Former-commit-id: 7ed38d63558c0dc73b8f55cea5d643986b7e798b

--- public/java/src/uk/ac/ox/well/indiana/commands/simulate/FindVariantsInContigs.java
+++ public/java/src/uk/ac/ox/well/indiana/commands/simulate/FindVariantsInContigs.java
@@ -12,6 +12,7 @@ import htsjdk.samtools.util.IntervalTreeMap;
 import htsjdk.variant.variantcontext.VariantContext;
 import htsjdk.variant.variantcontext.VariantContextBuilder;
 import htsjdk.variant.vcf.VCFFileReader;
+import org.apache.commons.lang.StringUtils;
 import uk.ac.ox.well.indiana.commands.Module;
 import uk.ac.ox.well.indiana.utils.arguments.Argument;
 import uk.ac.ox.well.indiana.utils.arguments.Output;
@@ -20,10 +21,9 @@ import uk.ac.ox.well.indiana.utils.sequence.SequenceUtils;
 
 import java.io.File;
 import java.io.PrintStream;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.TreeMap;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class FindVariantsInContigs extends Module {
     @Argument(fullName="contigs", shortName="c", doc="Contigs")
@@ -31,9 +31,6 @@ public class FindVariantsInContigs extends Module {
 
     @Argument(fullName="vcf", shortName="v", doc="VCF")
     public VCFFileReader VCF;
-
-    @Argument(fullName="fastq", shortName="f", doc="Fastq flanks end 1")
-    public File FASTQ;
 
     @Argument(fullName="bam", shortName="b", doc="BAM")
     public SAMFileReader BAM;
@@ -51,43 +48,20 @@ public class FindVariantsInContigs extends Module {
             String seq = new String(rseq.getBases());
 
             contigSequences.put(name[0], seq);
-
-            //log.info("  {}={}", name[0], seq.length());
         }
         log.info("  loaded {} sequences", contigSequences.size());
 
         log.info("Loading variants...");
-        IntervalTreeMap<VariantContext> variants = new IntervalTreeMap<VariantContext>();
+        Map<String, VariantContext> variants = new HashMap<String, VariantContext>();
         for (VariantContext vc : VCF) {
-            Interval interval = new Interval(vc.getChr(), vc.getStart(), vc.getStart());
-
-            variants.put(interval, vc);
+            variants.put(vc.getAttributeAsString("SIMID", "unknown"), vc);
         }
         log.info("  loaded {} variants", variants.size());
-
-        log.info("Loading fastq info...");
-        Map<String, VariantContext> vinfo = new HashMap<String, VariantContext>();
-        FastqReader fqr1 = new FastqReader(FASTQ);
-
-        while (fqr1.hasNext()) {
-            FastqRecord fqr = fqr1.next();
-
-            String[] pieces = fqr.getReadHeader().split("[\\s:]+");
-            String chr = pieces[1];
-            int start = Integer.valueOf(pieces[2]);
-
-            Interval interval = new Interval(chr, start, start);
-
-            VariantContext vc = variants.get(interval);
-
-            vinfo.put(pieces[0], vc);
-        }
-        log.info("  loaded {} records", vinfo.size());
 
         log.info("Loading flank alignments...");
         int numBothAligned = 0, numSameContig = 0, numUnambiguous = 0;
         for (SAMRecord sr : BAM) {
-            VariantContext vc = vinfo.get(sr.getReadName());
+            VariantContext vc = variants.get(sr.getReadName());
             VariantContext newvc;
 
             if (sr.getFirstOfPairFlag()) {
@@ -115,89 +89,170 @@ public class FindVariantsInContigs extends Module {
                 }
             }
 
-            vinfo.put(sr.getReadName(), newvc);
+            variants.put(sr.getReadName(), newvc);
         }
         log.info("  {} records with both flanks mapped", numBothAligned);
         log.info("  {} records with both flanks mapped and on the same contig", numSameContig);
         log.info("  {} records with both flanks mapped, on the same contig, with high MQ", numUnambiguous);
 
         log.info("Examining recovered contigs...");
-        int numRecoveredVariants = 0;
-        //Map<String, Integer> knownTypes = new HashMap<String, Integer>();
-        //Map<String, Integer> recoveredTypes = new HashMap<String, Integer>();
-        Map<String, Map<Integer, Integer>> knownTypes = new TreeMap<String, Map<Integer, Integer>>();
-        Map<String, Map<Integer, Integer>> recoveredTypes = new TreeMap<String, Map<Integer, Integer>>();
+        Map<String, Integer> knownTypes = new HashMap<String, Integer>();
+        TableWriter tw = new TableWriter(out);
 
-        for (String id : vinfo.keySet()) {
-            VariantContext newvc = vinfo.get(id);
+        Map<String, Set<VariantContext>> gcVariants = new TreeMap<String, Set<VariantContext>>();
+        Set<String> idsToRemove = new HashSet<String>();
+        for (String id : variants.keySet()) {
+            VariantContext newvc = variants.get(id);
 
-            String type = newvc.getAttributeAsString("DENOVO", "unknown");
-            int length = type.equals("DEL") ? newvc.getReference().length() - 1 : newvc.getAlternateAllele(0).length();
+            String gcid = newvc.getAttributeAsString("GCINDEX", "NA");
+            String nahr = newvc.getAttributeAsString("NAHR", "NA");
+            String denovo = newvc.getAttributeAsString("DENOVO", "NA");
 
-            if (type.equals("STR_CON")) {
-                log.info("  str_con={}", newvc);
+            if (!gcid.equals("NA")) {
+                String totalId = newvc.getChr() + ".gcid" + gcid;
+
+                if (!gcVariants.containsKey(totalId)) {
+                    gcVariants.put(totalId, new HashSet<VariantContext>());
+                }
+
+                gcVariants.get(totalId).add(newvc);
+
+                idsToRemove.add(id);
+            } else if (!nahr.equals("NA")) {
+                idsToRemove.add(id);
+            } else if (denovo.equals("RECOMB")) {
+                idsToRemove.add(id);
+            }
+        }
+
+        for (String totalId : gcVariants.keySet()) {
+            //log.info("  id={}", totalId);
+
+            for (VariantContext newvc : gcVariants.get(totalId)) {
+                //log.info("    vc={}", newvc);
+            }
+        }
+
+        for (String id : idsToRemove) {
+            variants.remove(id);
+        }
+
+        for (String id : variants.keySet()) {
+            VariantContext newvc = variants.get(id);
+
+            String denovo = newvc.getAttributeAsString("DENOVO", "NA");
+            String nahr = newvc.getAttributeAsString("NAHR", "NA");
+            String event = nahr.equals("NA") ? denovo : "NAHR";
+            String type = newvc.getType().name();
+            String gcid = newvc.getAttributeAsString("GCINDEX", "NA");
+            String repUnit = newvc.getAttributeAsString("REPUNIT", "NA");
+            int repsBefore = newvc.getAttributeAsInt("REPS_BEFORE", 0);
+            int repsAfter = newvc.getAttributeAsInt("REPS_AFTER", 0);
+            boolean variantFound = false;
+            String matchedSeq = "NA";
+            int matchedPos = 0;
+            int length = 0;
+
+            if (newvc.isSimpleInsertion()) {
+                length = newvc.getAlternateAllele(0).length() - newvc.getReference().length();
+            } else if (newvc.isSimpleDeletion()) {
+                length = newvc.getReference().length() - newvc.getAlternateAllele(0).length();
+            } else if (newvc.isMNP()) {
+                length = newvc.getReference().length();
             }
 
-            if (!knownTypes.containsKey(type)) {
-                knownTypes.put(type, new TreeMap<Integer, Integer>());
-            }
+            SAMRecord end1 = newvc.hasAttribute("left_flank_read") ? (SAMRecord) newvc.getAttribute("left_flank_read") : null;
+            SAMRecord end2 = newvc.hasAttribute("right_flank_read") ? (SAMRecord) newvc.getAttribute("right_flank_read") : null;
 
-            if (!knownTypes.get(type).containsKey(length)) {
-                knownTypes.get(type).put(length, 1);
-            } else {
-                knownTypes.get(type).put(length, knownTypes.get(type).get(length) + 1);
-            }
+            if (end1 != null && end2 != null &&
+                end1.getReferenceName().equals(end2.getReferenceName()) &&
+                end1.getMappingQuality() > 0 && end2.getMappingQuality() > 0) {
 
-            if (newvc.hasAttribute("left_flank_read") && newvc.hasAttribute("right_flank_read")) {
-                SAMRecord end1 = (SAMRecord) newvc.getAttribute("left_flank_read");
-                SAMRecord end2 = (SAMRecord) newvc.getAttribute("right_flank_read");
+                String contig = contigSequences.get(end1.getReferenceName());
 
-                if (end1.getReferenceName().equals(end2.getReferenceName()) &&
-                    end1.getMappingQuality() > 0 && end2.getMappingQuality() > 0) {
+                String leftFlankFw  = newvc.getAttributeAsString("flank_left", "N");
+                String altAlleleFw  = newvc.isVariant() ? newvc.getAlternateAllele(0).getBaseString() : newvc.getReference().getBaseString();
+                String rightFlankFw = newvc.getAttributeAsString("flank_right", "N");
+                String patternFw    = ".*(" + leftFlankFw + ".*" + altAlleleFw + ".*" + rightFlankFw + ").*";
 
-                    String contig = contigSequences.get(end1.getReferenceName());
+                String leftFlankRc  = SequenceUtils.reverseComplement(leftFlankFw);
+                String altAlleleRc  = SequenceUtils.reverseComplement(altAlleleFw);
+                String rightFlankRc = SequenceUtils.reverseComplement(rightFlankFw);
+                String patternRc    = ".*(" + rightFlankRc + ".*" + altAlleleRc + ".*" + leftFlankRc + ").*";
 
-                    String leftFlankFw  = newvc.getAttributeAsString("flank_left", "N");
-                    String altAlleleFw  = newvc.getAlternateAllele(0).getBaseString();
-                    String rightFlankFw = newvc.getAttributeAsString("flank_right", "N");
+                if (contig.contains(leftFlankFw) && contig.contains(rightFlankFw)) {
+                    int leftFlankIndex = contig.indexOf(leftFlankFw);
+                    int rightFlankIndex = contig.indexOf(rightFlankFw);
 
-                    String leftFlankRc  = SequenceUtils.reverseComplement(leftFlankFw);
-                    String altAlleleRc  = SequenceUtils.reverseComplement(altAlleleFw);
-                    String rightFlankRc = SequenceUtils.reverseComplement(rightFlankFw);
+                    int start = (leftFlankIndex < rightFlankIndex) ? leftFlankIndex : rightFlankIndex;
+                    int stop = leftFlankFw.length() + ((leftFlankIndex < rightFlankIndex) ? rightFlankIndex : leftFlankIndex);
 
-                    if ((contig.contains(leftFlankFw) && contig.contains(altAlleleFw) && contig.contains(rightFlankFw)) ||
-                        (contig.contains(leftFlankRc) && contig.contains(altAlleleRc) && contig.contains(rightFlankRc))) {
-                        numRecoveredVariants++;
+                    String subcontig = contig.substring(start, stop);
 
-                        if (!recoveredTypes.containsKey(type)) {
-                            recoveredTypes.put(type, new TreeMap<Integer, Integer>());
-                        }
+                    Pattern pFw = Pattern.compile(patternFw);
+                    Matcher mFw = pFw.matcher(subcontig);
 
-                        if (!recoveredTypes.get(type).containsKey(length)) {
-                            recoveredTypes.get(type).put(length, 1);
-                        } else {
-                            recoveredTypes.get(type).put(length, recoveredTypes.get(type).get(length) + 1);
-                        }
+                    if (mFw.matches()) {
+                        matchedSeq = mFw.group(1);
+                        matchedPos = mFw.start(1) + start;
+
+                        variantFound = true;
+                    }
+                } else if (contig.contains(leftFlankRc) && contig.contains(rightFlankRc)) {
+                    int leftFlankIndex = contig.indexOf(leftFlankRc);
+                    int rightFlankIndex = contig.indexOf(rightFlankRc);
+
+                    int start = (leftFlankIndex < rightFlankIndex) ? leftFlankIndex : rightFlankIndex;
+                    int stop = leftFlankRc.length() + ((leftFlankIndex < rightFlankIndex) ? rightFlankIndex : leftFlankIndex);
+
+                    String subcontig = contig.substring(start, stop);
+                    Pattern pRc = Pattern.compile(patternRc);
+                    Matcher mRc = pRc.matcher(subcontig);
+
+                    if (mRc.matches()) {
+                        matchedSeq = mRc.group(1);
+                        matchedPos = mRc.start(1) + start;
+
+                        variantFound = true;
                     }
                 }
             }
-        }
-        log.info("  {} total recovered", numRecoveredVariants);
-        //log.info("  knownTypes={}", Joiner.on(",").withKeyValueSeparator("=").join(knownTypes));
-        //log.info("  recoveredTypes={}", Joiner.on(",").withKeyValueSeparator("=").join(recoveredTypes));
 
-        TableWriter tw = new TableWriter(out);
-        for (String type : knownTypes.keySet()) {
-            for (int length : knownTypes.get(type).keySet()) {
-                Map<String, String> te = new LinkedHashMap<String, String>();
+            Map<String, String> te = new LinkedHashMap<String, String>();
+            te.put("id", id);
+            te.put("event", event);
+            te.put("type", type);
+            te.put("length", String.valueOf(length));
+            te.put("variantFound", variantFound ? "TRUE" : "FALSE");
+            te.put("matchedPos", String.valueOf(matchedPos));
+            te.put("chr", newvc.getChr());
+            te.put("start", String.valueOf(newvc.getStart()));
+            te.put("gcindex", gcid);
+            te.put("nahr", nahr);
+            te.put("repUnit", repUnit);
+            te.put("repsBefore", String.valueOf(repsBefore));
+            te.put("repsAfter", String.valueOf(repsAfter));
+            te.put("flank1", end1 == null ? "NA" : end1.getReadString());
+            te.put("flank2", end2 == null ? "NA" : end2.getReadString());
+            te.put("mq1", end1 == null ? "NA" : String.valueOf(end1.getMappingQuality()));
+            te.put("mq2", end2 == null ? "NA" : String.valueOf(end2.getMappingQuality()));
+            te.put("contig1", end1 == null ? "NA" : end1.getReferenceName());
+            te.put("contig2", end2 == null ? "NA" : end2.getReferenceName());
+            te.put("pos1", end1 == null ? "NA" : String.valueOf(end1.getAlignmentStart()));
+            te.put("pos2", end2 == null ? "NA" : String.valueOf(end2.getAlignmentStart()));
+            te.put("matchedSeq", matchedSeq);
 
-                te.put("type", type);
-                te.put("length", String.valueOf(length));
-                te.put("known", String.valueOf(knownTypes.get(type).get(length)));
-                te.put("recovered", String.valueOf(recoveredTypes.get(type).get(length)));
-
+            if (!event.equals("NA")) {
                 tw.addEntry(te);
+
+                if (!knownTypes.containsKey(event)) {
+                    knownTypes.put(event, 1);
+                } else {
+                    knownTypes.put(event, knownTypes.get(event) + 1);
+                }
             }
         }
+
+        log.info("  {}", Joiner.on(", ").withKeyValueSeparator("=").join(knownTypes));
     }
 }
