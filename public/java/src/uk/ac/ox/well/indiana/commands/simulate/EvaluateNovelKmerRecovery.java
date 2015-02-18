ccd0e18c90945f2d56c2724c4bccedabc6c72323
Kiran V Garimella <kiran@well.ox.ac.uk>
2015-02-18 15:00:46 +0000

    Better stratification of novel kmers into variant classes

--- public/java/src/uk/ac/ox/well/indiana/commands/simulate/EvaluateNovelKmerRecovery.java
+++ public/java/src/uk/ac/ox/well/indiana/commands/simulate/EvaluateNovelKmerRecovery.java
@@ -1,6 +1,7 @@
 package uk.ac.ox.well.indiana.commands.simulate;
 
 import com.google.common.base.Joiner;
+import htsjdk.samtools.reference.FastaSequenceFile;
 import htsjdk.samtools.reference.IndexedFastaSequenceFile;
 import htsjdk.samtools.reference.ReferenceSequence;
 import htsjdk.variant.variantcontext.VariantContext;
@@ -21,6 +22,12 @@ import java.util.*;
 public class EvaluateNovelKmerRecovery extends Module {
     @Argument(fullName="reference", shortName="r", doc="Reference")
     public IndexedFastaSequenceFile REFERENCE;
+
+    @Argument(fullName="ref0", shortName="r0", doc="Reference parent 0")
+    public FastaSequenceFile REF0;
+
+    @Argument(fullName="ref1", shortName="r1", doc="Reference parent 1")
+    public FastaSequenceFile REF1;
 
     @Argument(fullName="bed", shortName="b", doc="Bed file")
     public File BED;
@@ -73,6 +80,34 @@ public class EvaluateNovelKmerRecovery extends Module {
                 }
             }
         }
+
+        while ((rseq = REF0.nextSequence()) != null) {
+            String seq = new String(rseq.getBases());
+
+            for (int i = 0; i <= seq.length() - GRAPH.getKmerSize(); i++) {
+                CortexKmer ck = new CortexKmer(seq.substring(i, i + GRAPH.getKmerSize()));
+
+                if (!kmerCount.containsKey(ck)) {
+                    kmerCount.put(ck, 1);
+                } else {
+                    kmerCount.put(ck, kmerCount.get(ck) + 1);
+                }
+            }
+        }
+
+        while ((rseq = REF1.nextSequence()) != null) {
+            String seq = new String(rseq.getBases());
+
+            for (int i = 0; i <= seq.length() - GRAPH.getKmerSize(); i++) {
+                CortexKmer ck = new CortexKmer(seq.substring(i, i + GRAPH.getKmerSize()));
+
+                if (!kmerCount.containsKey(ck)) {
+                    kmerCount.put(ck, 1);
+                } else {
+                    kmerCount.put(ck, kmerCount.get(ck) + 1);
+                }
+            }
+        }
         log.info("  {} kmers", kmerCount.size());
 
         TableReader tr = new TableReader(BED, new String[] {"chrom", "start", "stop", "info"});
@@ -80,8 +115,7 @@ public class EvaluateNovelKmerRecovery extends Module {
         Map<String, Map<String, String>> variants = new HashMap<String, Map<String, String>>();
         Map<CortexKmer, KmerEntry> variantKmers = new HashMap<CortexKmer, KmerEntry>();
 
-        log.info("Loading kmers from variants...");
-        int kmers = 0;
+        log.info("Loading novel kmers from variants...");
         for (Map<String, String> te : tr) {
             Map<String, String> infoMap = parseInfoField(te.get("info"));
 
@@ -103,11 +137,26 @@ public class EvaluateNovelKmerRecovery extends Module {
 
                 if (kmerCount.containsKey(ck) && kmerCount.get(ck) == 1) {
                     variantKmers.put(ck, new KmerEntry(infoMap.get("id")));
-                    kmers++;
                 }
             }
         }
-        log.info("  {} kmers", kmers);
+
+        log.info("  {} kmers", variantKmers.size());
+
+        log.info("Loading other novel kmers...");
+        int moreKmers = 0;
+        while ((rseq = REFERENCE.nextSequence()) != null) {
+            String seq = new String(rseq.getBases());
+
+            for (int i = 0; i <= seq.length() - GRAPH.getKmerSize(); i++) {
+                CortexKmer ck = new CortexKmer(seq.substring(i, i + GRAPH.getKmerSize()));
+
+                if (kmerCount.containsKey(ck) && kmerCount.get(ck) == 1 && !variantKmers.containsKey(ck)) {
+                    variantKmers.put(ck, new KmerEntry("other"));
+                }
+            }
+        }
+        log.info("  {} kmers", moreKmers);
 
         log.info("Looking for novel kmers in graph...");
         int novelKmers = 0;
@@ -131,10 +180,6 @@ public class EvaluateNovelKmerRecovery extends Module {
             if (variantKmers.containsKey(cr.getCortexKmer())) {
                 variantKmers.get(cr.getCortexKmer()).found = true;
                 variantKmers.get(cr.getCortexKmer()).coverage = cr.getCoverage(childColor);
-            } else {
-                variantKmers.put(cr.getCortexKmer(), new KmerEntry("other"));
-                variantKmers.get(cr.getCortexKmer()).found = true;
-                variantKmers.get(cr.getCortexKmer()).coverage = cr.getCoverage(childColor);
             }
 
             if (isNovelKmer) {
@@ -142,6 +187,11 @@ public class EvaluateNovelKmerRecovery extends Module {
 
                 if (variantKmers.containsKey(cr.getCortexKmer())) {
                     variantKmers.get(cr.getCortexKmer()).novel = true;
+                } else {
+                    variantKmers.put(cr.getCortexKmer(), new KmerEntry("other"));
+                    variantKmers.get(cr.getCortexKmer()).found = true;
+                    variantKmers.get(cr.getCortexKmer()).novel = true;
+                    variantKmers.get(cr.getCortexKmer()).coverage = cr.getCoverage(childColor);
                 }
             }
         }
@@ -151,37 +201,31 @@ public class EvaluateNovelKmerRecovery extends Module {
         Map<String, Integer> expected = new HashMap<String, Integer>();
         Map<String, Integer> found = new HashMap<String, Integer>();
         Map<String, Integer> novel = new HashMap<String, Integer>();
-        //Map<String, Integer> cov5 = new HashMap<String, Integer>();
-        //Map<String, Integer> cov10 = new HashMap<String, Integer>();
-        //Map<String, Integer> cov15 = new HashMap<String, Integer>();
-        //Map<String, Integer> cov20 = new HashMap<String, Integer>();
-        //Map<String, Integer> cov25 = new HashMap<String, Integer>();
-        //Map<String, Integer> cov30 = new HashMap<String, Integer>();
 
+        int kmersSeen = 0;
         for (CortexKmer ck : variantKmers.keySet()) {
+            kmersSeen++;
+
             String variantId = variantKmers.get(ck).variantId;
             Map<String, String> te = variantId.equals("other") ? null : variants.get(variantId);
-            Map<String, String> variantInfo = variantId.equals("other") ? null : parseInfoField(te.get("info"));
+            Map<String, String> variantInfo = te == null ? null : parseInfoField(te.get("info"));
 
-            //String denovo = variantInfo.get("denovo");
-            //int length = 0;
-            //if (denovo.equals("INS") || denovo.equals("TD") || denovo.equals("STR_EXP")) { length = variantInfo.get("alt").length() - 1; }
-            //if (denovo.equals("DEL") || denovo.equals("STR_CON")) { length = variantInfo.get("ref").length() - 1; }
-            //if (denovo.equals("INV")) { length = variantInfo.get("ref").length(); }
-            //String vclass = denovo + "." + length;
+            String vclass = variantInfo == null ? "other" : variantInfo.get("denovo");
 
-            String vclass = variantId.equals("other") ? "other" : variantInfo.get("denovo");
+            if (variantInfo != null && !variantInfo.get("nahr").equals("unknown")) {
+                vclass = "NAHR";
+            }
+
+            if (vclass.equals("unknown")) {
+                if (variantInfo != null) {
+                    vclass = "inherited_" + variantInfo.get("type");
+                }
+            }
 
             if (!expected.containsKey(vclass)) {
                 expected.put(vclass, 0);
                 found.put(vclass, 0);
                 novel.put(vclass, 0);
-                //cov5.put(vclass, 0);
-                //cov10.put(vclass, 0);
-                //cov15.put(vclass, 0);
-                //cov20.put(vclass, 0);
-                //cov25.put(vclass, 0);
-                //cov30.put(vclass, 0);
             }
 
             expected.put(vclass, expected.get(vclass) + 1);
@@ -190,20 +234,10 @@ public class EvaluateNovelKmerRecovery extends Module {
 
                 if (variantKmers.get(ck).novel) {
                     novel.put(vclass, novel.get(vclass) + 1);
-
-                    //if (variantKmers.get(ck).coverage >= 5)  { cov5.put(vclass, cov5.get(vclass) + 1); }
-                    //if (variantKmers.get(ck).coverage >= 10) { cov10.put(vclass, cov10.get(vclass) + 1); }
-                    //if (variantKmers.get(ck).coverage >= 15) { cov15.put(vclass, cov15.get(vclass) + 1); }
-                    //if (variantKmers.get(ck).coverage >= 20) { cov20.put(vclass, cov20.get(vclass) + 1); }
-                    //if (variantKmers.get(ck).coverage >= 25) { cov25.put(vclass, cov25.get(vclass) + 1); }
-                    //if (variantKmers.get(ck).coverage >= 30) { cov30.put(vclass, cov30.get(vclass) + 1); }
                 }
             }
         }
-
-        //log.info("    exp={}", Joiner.on(", ").withKeyValueSeparator("=").useForNull("null").join(expected));
-        //log.info("  found={}", Joiner.on(", ").withKeyValueSeparator("=").useForNull("null").join(found));
-        //log.info("  novel={}", Joiner.on(", ").withKeyValueSeparator("=").useForNull("null").join(novel));
+        log.info("  {} kmers", kmersSeen);
 
         TableWriter tw = new TableWriter(out);
 
@@ -213,16 +247,9 @@ public class EvaluateNovelKmerRecovery extends Module {
             String[] pieces = vclass.split("\\.");
 
             te.put("class", pieces[0]);
-            te.put("length", (pieces.length == 2) ? pieces[1] : "0");
             te.put("expected", String.valueOf(expected.get(vclass)));
             te.put("found", String.valueOf(found.get(vclass)));
             te.put("novel", String.valueOf(novel.get(vclass)));
-            //te.put("cov5", String.valueOf(cov5.get(vclass)));
-            //te.put("cov10", String.valueOf(cov10.get(vclass)));
-            //te.put("cov15", String.valueOf(cov15.get(vclass)));
-            //te.put("cov20", String.valueOf(cov20.get(vclass)));
-            //te.put("cov25", String.valueOf(cov25.get(vclass)));
-            //te.put("cov30", String.valueOf(cov30.get(vclass)));
 
             tw.addEntry(te);
         }
