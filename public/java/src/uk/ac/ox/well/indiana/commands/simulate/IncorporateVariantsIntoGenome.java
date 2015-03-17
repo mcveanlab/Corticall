f829dc31581303d1a98dedb2e59413ec01f8e895
Kiran V Garimella <kiran@well.ox.ac.uk>
2015-03-17 17:50:46 +0000

    Output read start distribution across genome, rather than coverage distribution
    Former-commit-id: 5c78baf3fd34c9cf5ec2eaef27db4ab6beb790cf

--- public/java/src/uk/ac/ox/well/indiana/commands/simulate/IncorporateVariantsIntoGenome.java
+++ public/java/src/uk/ac/ox/well/indiana/commands/simulate/IncorporateVariantsIntoGenome.java
@@ -1,6 +1,9 @@
 package uk.ac.ox.well.indiana.commands.simulate;
 
 import com.google.common.base.Joiner;
+import htsjdk.samtools.SAMFileReader;
+import htsjdk.samtools.SAMRecord;
+import htsjdk.samtools.SAMRecordIterator;
 import htsjdk.samtools.reference.FastaSequenceFile;
 import htsjdk.samtools.reference.IndexedFastaSequenceFile;
 import htsjdk.samtools.reference.ReferenceSequence;
@@ -23,6 +26,9 @@ public class IncorporateVariantsIntoGenome extends Module {
     @Argument(fullName="reference", shortName="r", doc="Reference")
     public FastaSequenceFile REF;
 
+    @Argument(fullName="refCoverage", shortName="c", doc="BAM file from which to extract coverage info")
+    public SAMFileReader COV;
+
     @Argument(fullName="vcf", shortName="v", doc="VCF")
     public VCFFileReader VCF;
 
@@ -40,6 +46,9 @@ public class IncorporateVariantsIntoGenome extends Module {
 
     @Output(fullName="bedout", shortName="bo", doc="Bed out")
     public PrintStream bout;
+
+    @Output(fullName="covout", shortName="co", doc="Coverage out")
+    public PrintStream cout;
 
     @Override
     public void execute() {
@@ -92,6 +101,21 @@ public class IncorporateVariantsIntoGenome extends Module {
                     }
                 }
             }
+
+            SAMRecordIterator sri = COV.queryOverlapping(name, 0, rseq.length());
+            int[] covs = new int[rseq.length()];
+
+            while (sri.hasNext()) {
+                SAMRecord read = sri.next();
+
+                /*for (int i = read.getAlignmentStart(); i < read.getAlignmentEnd(); i++) {
+                    covs[i - 1]++;
+                }*/
+
+                covs[read.getAlignmentStart() - 1]++;
+            }
+
+            sri.close();
 
             int gpos = 0;
             for (int i = 0; i < seq.length(); i++) {
@@ -147,6 +171,12 @@ public class IncorporateVariantsIntoGenome extends Module {
 
                         bout.println(Joiner.on("\t").join(name, gpos, gpos + length, bedname));
                     }
+                }
+
+                int cov = covs[i];
+
+                for (int q = 0; q < alleles.get(i).length(); q++) {
+                    cout.println(Joiner.on("\t").join(name, gpos + q, cov));
                 }
 
                 gpos += alleles.get(i).length();
