a388cec4ee9a91210c61530837879e9f4d08e914
Kiran V Garimella <kiran@well.ox.ac.uk>
2015-03-19 15:25:42 +0000

    Return positions of mismatches, insertions, and deletions in a read.

---
+++ public/java/src/uk/ac/ox/well/indiana/utils/sequence/AlignmentUtils.java
@@ -0,0 +1,103 @@
+package uk.ac.ox.well.indiana.utils.sequence;
+
+import htsjdk.samtools.CigarElement;
+import htsjdk.samtools.CigarOperator;
+import htsjdk.samtools.SAMRecord;
+import uk.ac.ox.well.indiana.utils.exceptions.IndianaException;
+
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class AlignmentUtils {
+    private AlignmentUtils() {}
+
+    private static Pattern p = Pattern.compile("[0-9]+|[A-Z]|\\^[A-Z]+");
+
+    private static int computeInitialPosition(SAMRecord read) {
+        int currentPosition = 0;
+
+        if (read.getCigar().getCigarElements().size() > 0 && read.getCigar().getCigarElement(0).getOperator().equals(CigarOperator.S)) {
+            currentPosition = read.getCigar().getCigarElement(0).getLength();
+        }
+
+        return currentPosition;
+    }
+
+    public static Set<Integer> getInsertionPositions(SAMRecord read) {
+        int currentPosition = computeInitialPosition(read);
+
+        Set<Integer> insertionPositions = new TreeSet<Integer>();
+
+        for (CigarElement ce : read.getCigar().getCigarElements()) {
+            if (!ce.getOperator().equals(CigarOperator.H) && !ce.getOperator().equals(CigarOperator.S)) {
+                if (ce.getOperator().equals(CigarOperator.I)) {
+                    insertionPositions.add(currentPosition);
+                }
+
+                currentPosition += ce.getLength();
+            }
+        }
+
+        return insertionPositions;
+    }
+
+    public static Set<Integer> getDeletionPositions(SAMRecord read) {
+        int currentPosition = computeInitialPosition(read);
+
+        Set<Integer> deletionPositions = new TreeSet<Integer>();
+
+        for (CigarElement ce : read.getCigar().getCigarElements()) {
+            if (!ce.getOperator().equals(CigarOperator.H) && !ce.getOperator().equals(CigarOperator.S)) {
+                if (ce.getOperator().equals(CigarOperator.D)) {
+                    deletionPositions.add(currentPosition);
+                }
+
+                currentPosition += ce.getLength();
+            }
+        }
+
+        return deletionPositions;
+    }
+
+    public static Set<Integer> getMismatchPositions(SAMRecord read) {
+        Set<Integer> mismatchPositions = new TreeSet<Integer>();
+
+        String md = read.getStringAttribute("MD");
+        if (md != null) {
+            Matcher m = p.matcher(md);
+
+            int currentPosition = computeInitialPosition(read);
+
+            if (read.getCigar().getCigarElements().size() > 0 && read.getCigar().getCigarElement(0).getOperator().equals(CigarOperator.S)) {
+                currentPosition = read.getCigar().getCigarElement(0).getLength();
+            }
+
+            while (m.find()) {
+                String piece = md.substring(m.start(), m.end());
+
+                if (!piece.contains("^")) {
+                    if (piece.matches("[A-Z]")) {
+                        mismatchPositions.add(currentPosition);
+                        currentPosition++;
+                    } else {
+                        currentPosition += Integer.parseInt(piece);
+                    }
+                }
+            }
+        }
+
+        return mismatchPositions;
+    }
+
+    public static Set<Integer> getDifferencesPositions(SAMRecord read) {
+        Set<Integer> diffPositions = new TreeSet<Integer>();
+
+        diffPositions.addAll(getMismatchPositions(read));
+        diffPositions.addAll(getInsertionPositions(read));
+        diffPositions.addAll(getDeletionPositions(read));
+
+        return diffPositions;
+    }
+}
