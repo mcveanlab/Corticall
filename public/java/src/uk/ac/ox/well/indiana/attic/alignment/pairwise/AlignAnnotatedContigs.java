b016829616a87369c1cd97c7d29516f1a83683a7
Kiran V Garimella <kiran@well.ox.ac.uk>
2015-04-07 14:10:45 +0100

    Checkpoint: implemented pair-HMM for global sequence alignment

--- public/java/src/uk/ac/ox/well/indiana/attic/alignment/pairwise/AlignAnnotatedContigs.java
+++ public/java/src/uk/ac/ox/well/indiana/attic/alignment/pairwise/AlignAnnotatedContigs.java
@@ -13,11 +13,14 @@ import uk.ac.ox.well.indiana.utils.arguments.Output;
 import uk.ac.ox.well.indiana.utils.exceptions.IndianaException;
 import uk.ac.ox.well.indiana.utils.io.cortex.graph.CortexKmer;
 import uk.ac.ox.well.indiana.utils.io.table.TableReader;
+import uk.ac.ox.well.indiana.utils.math.MoreMathUtils;
 import uk.ac.ox.well.indiana.utils.sequence.SequenceUtils;
 
 import java.io.File;
 import java.io.PrintStream;
 import java.util.*;
+
+import static uk.ac.ox.well.indiana.utils.math.MoreMathUtils.max;
 
 public class AlignAnnotatedContigs extends Module {
     @Argument(fullName="metrics", shortName="m", doc="Contig metrics")
@@ -25,6 +28,12 @@ public class AlignAnnotatedContigs extends Module {
 
     @Argument(fullName="reference", shortName="r", doc="Reference sequence")
     public IndexedFastaSequenceFile REFERENCE;
+
+    @Argument(fullName="delta", shortName="tpd", doc="Transition probability for first gap")
+    public Double DELTA = 0.2;
+
+    @Argument(fullName="epsilon", shortName="tpe", doc="Transition probability for remaining in gap")
+    public Double EPSILON = 0.2;
 
     @Argument(fullName="kmerSize", shortName="k", doc="Kmer size")
     public Integer KMER_SIZE = 47;
@@ -132,8 +141,116 @@ public class AlignAnnotatedContigs extends Module {
         return (candidateInterval.isNegativeStrand()) ? candidateSeqRc : candidateSeqFw;
     }
 
-    private void align(String seq, String candidateSeq) {
+    private void printMatrix(String query, String target, double[][] m) {
+        log.info("   {}", Joiner.on(" ").join(Arrays.copyOfRange(target.split(""), 1, target.length() - 1)));
 
+        for (int i = 0; i < m.length; i++) {
+            List<Double> values = new ArrayList<Double>();
+            for (int j = 0; j < m[0].length; j++) {
+                values.add(m[i][j]);
+            }
+
+            log.info("{} {}", query.charAt(i), Joiner.on(" ").join(values));
+        }
+    }
+
+    private int whichMax(double... values) {
+        int maxIndex = -1;
+        double maxValue = Double.MIN_VALUE;
+
+        for (int i = 0; i < values.length; i++) {
+            if (values[i] > maxValue) {
+                maxIndex = i;
+                maxValue = values[i];
+            }
+        }
+
+        return maxIndex;
+    }
+
+    private void viterbi(String query, String target, double[][] tm, double[][] em) {
+        double[][] vm = new double[query.length()][target.length()];
+        double[][] vi = new double[query.length()][target.length()];
+        double[][] vd = new double[query.length()][target.length()];
+
+        vm[0][0] = 1;
+        for (int i = 0; i < query.length(); i++) {
+            for (int j = 0; j < target.length(); j++) {
+                if (i != 0 && j != 0) {
+                    vm[i][j] = em[0][0] * MoreMathUtils.max(
+                                 (1 - 2*DELTA)*vm[i - 1][j - 1],
+                                 (1 - EPSILON)*vi[i - 1][j - 1],
+                                 (1 - EPSILON)*vd[i - 1][j - 1]
+                    );
+
+                    vi[i][j] = em[1][1] * MoreMathUtils.max(
+                                 (DELTA)*vm[i - 1][j],
+                                 (EPSILON)*vi[i - 1][j]
+                    );
+
+                    vd[i][j] = em[2][2] * MoreMathUtils.max(
+                                 (DELTA)*vm[i][j - 1],
+                                 (EPSILON)*vd[i][j - 1]
+                    );
+                }
+            }
+        }
+
+        StringBuilder qa = new StringBuilder();
+        StringBuilder ta = new StringBuilder();
+
+        int i = query.length() - 1;
+        int j = target.length() - 1;
+
+        while (i >= 0 && j >= 0) {
+            switch (whichMax(vm[i][j], vi[i][j], vd[i][j])) {
+                case 0:
+                    qa.insert(0, query.charAt(i));
+                    ta.insert(0, target.charAt(j));
+                    i -= 1;
+                    j -= 1;
+                    break;
+                case 1:
+                    qa.insert(0, query.charAt(i));
+                    ta.insert(0, "-");
+                    i -= 1;
+                    break;
+                case 2:
+                    qa.insert(0, "-");
+                    ta.insert(0, target.charAt(j));
+                    j -= 1;
+                    break;
+            }
+        }
+
+        //printMatrix(query, target, vm);
+        //printMatrix(query, target, vi);
+        //printMatrix(query, target, vd);
+
+        log.debug("    swt: {}", ta.toString());
+        log.debug("    swq: {}", qa.toString());
+    }
+
+    private void align(String query, String target) {
+        // Define states
+        String[] states = new String[] { "M", "I", "D" };
+
+        // Define emissions
+        String[] emissions = new String[] { "+1,+1", "+1,+0", "+0,+1" };
+
+        // Define transition matrix
+        double[][] tm = new double[3][3];
+        tm[0] = new double[] { 1 - 2*DELTA, DELTA,   DELTA   };
+        tm[1] = new double[] { 1 - EPSILON, EPSILON, 0       };
+        tm[2] = new double[] { 1 - EPSILON, 0,       EPSILON };
+
+        // Define emission matrix
+        double[][] em = new double[3][3];
+        em[0] = new double[] { 1, 0, 0 };
+        em[1] = new double[] { 0, 1, 0 };
+        em[2] = new double[] { 0, 0, 1 };
+
+        viterbi(query, target, tm, em);
     }
 
     @Override
@@ -145,8 +262,8 @@ public class AlignAnnotatedContigs extends Module {
         TableReader tr = new TableReader(METRICS);
         int numRecords = 0;
         for (Map<String, String> te : tr) {
-            if (numRecords % (tr.size() / 10) == 0) {
-                log.info("  {}/{} records", numRecords, tr.size());
+            if (numRecords % (tr.size() / 5) == 0) {
+                log.info("  {}/{} (~{}%) records", numRecords, tr.size(), String.format("%.1f", (100.0f*numRecords) / (float) tr.size()));
             }
             numRecords++;
 
@@ -163,12 +280,15 @@ public class AlignAnnotatedContigs extends Module {
                 for (Interval candidateInterval : candidates) {
                     String candidateSeq = getAlignmentRegionCandidateSequence(candidateInterval);
 
-                    align(seq, candidateSeq);
-
                     log.debug("");
                     log.debug("    loc: {}", candidateInterval);
                     log.debug("    can: {}", candidateSeq);
                     log.debug("    seq: {}", seq);
+
+                    if (seq.length() < 200) {
+                        align(seq, candidateSeq);
+                    }
+
                     log.debug("");
                 }
 
@@ -176,6 +296,5 @@ public class AlignAnnotatedContigs extends Module {
                 log.debug("");
             }
         }
-        log.info("  {} records", numRecords);
     }
 }
