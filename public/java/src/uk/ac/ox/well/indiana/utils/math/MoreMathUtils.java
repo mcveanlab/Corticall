a4aa1a25b62588a567485d7384811a8ca1c1aa93
Kiran V Garimella <kiran@well.ox.ac.uk>
2015-04-07 15:02:01 +0100

    Refactored pair HMM aligner into GlobalAligner class
    Former-commit-id: e03537f6ff47831d3782b5a7b035465014ac2b1f

--- public/java/src/uk/ac/ox/well/indiana/utils/math/MoreMathUtils.java
+++ public/java/src/uk/ac/ox/well/indiana/utils/math/MoreMathUtils.java
@@ -13,6 +13,20 @@ public class MoreMathUtils {
         return max;
     }
 
+    public static int whichMax(double... values) {
+        int maxIndex = -1;
+        double maxValue = Double.MIN_VALUE;
+
+        for (int i = 0; i < values.length; i++) {
+            if (values[i] > maxValue) {
+                maxIndex = i;
+                maxValue = values[i];
+            }
+        }
+
+        return maxIndex;
+    }
+
     public static int roundUp(int n, int nearest) {
         return (n + nearest - 1) / nearest * nearest;
     }
