8656916a9b4318a46ef3dc5b8cc84c48cc1b22ad
Kiran V Garimella <kiran@well.ox.ac.uk>
2015-04-14 15:06:33 +0100

    Beginning of a tool to evaluate alignment quality
    Former-commit-id: dfed225a4185823ed36cf86385990f9fbb874b98

--- public/java/src/uk/ac/ox/well/indiana/attic/tools/sequence/AlignmentEval.java
+++ public/java/src/uk/ac/ox/well/indiana/attic/tools/sequence/AlignmentEval.java
@@ -1,7 +1,40 @@
 package uk.ac.ox.well.indiana.attic.tools.sequence;
 
-/**
- * Created by kiran on 14/02/2015.
- */
-public class AlignmentEval {
+import htsjdk.samtools.SAMFileReader;
+import htsjdk.samtools.SAMRecord;
+import htsjdk.samtools.SAMRecordIterator;
+import uk.ac.ox.well.indiana.commands.Module;
+import uk.ac.ox.well.indiana.utils.arguments.Argument;
+import uk.ac.ox.well.indiana.utils.arguments.Output;
+
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class AlignmentEval extends Module {
+    @Argument(fullName="baseline", shortName="b", doc="Baseline BAM")
+    public SAMFileReader BASELINE;
+
+    @Argument(fullName="comparisons", shortName="c", doc="Comparison BAM(s)")
+    public HashMap<String, SAMFileReader> COMPARISONS;
+
+    @Output
+    public PrintStream out;
+
+    @Override
+    public void execute() {
+        Map<String, SAMRecordIterator> sis = new HashMap<String, SAMRecordIterator>();
+        for (String key : COMPARISONS.keySet()) {
+            SAMFileReader sfr = COMPARISONS.get(key);
+            sis.put(key, sfr.iterator());
+        }
+
+        SAMRecordIterator bis = BASELINE.iterator();
+
+        while (bis.hasNext()) {
+
+        }
+    }
 }
