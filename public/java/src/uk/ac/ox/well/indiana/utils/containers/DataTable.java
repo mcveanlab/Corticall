f5b0a2f2a90fc1ae645cb6cfbb063c8a9d0f5ba5
Kiran V Garimella <kiran@well.ox.ac.uk>
2015-04-26 14:20:53 +0100

    Reduce the spacing between columns

--- public/java/src/uk/ac/ox/well/indiana/utils/containers/DataTable.java
+++ public/java/src/uk/ac/ox/well/indiana/utils/containers/DataTable.java
@@ -122,8 +122,8 @@ public class DataTable implements Iterable<Map<String, Object>>, Iterator<Map<St
 
         sb.append(tableName);
         for (String columnName : columnNames) {
-            int fieldWidth = columnWidths.get(columnName) + 2;
-            sb.append("  ");
+            int fieldWidth = columnWidths.get(columnName);
+            sb.append(" ");
             sb.append(String.format("%-" + fieldWidth + "s", columnName));
         }
         sb.append("\n");
@@ -134,7 +134,7 @@ public class DataTable implements Iterable<Map<String, Object>>, Iterator<Map<St
 
             for (String columnName : columnNames) {
                 String field = String.valueOf(get(primaryKey, columnName));
-                int fieldWidth = columnWidths.get(columnName) + 2;
+                int fieldWidth = columnWidths.get(columnName);
 
                 fields.add(String.format("%-" + fieldWidth + "s", field));
 
@@ -142,7 +142,7 @@ public class DataTable implements Iterable<Map<String, Object>>, Iterator<Map<St
             }
 
             //sb.append(Joiner.on("\t").join(fields)).append("\n");
-            sb.append(Joiner.on("  ").join(fields)).append("\n");
+            sb.append(Joiner.on(" ").join(fields)).append("\n");
         }
 
         sb.append("\n");
