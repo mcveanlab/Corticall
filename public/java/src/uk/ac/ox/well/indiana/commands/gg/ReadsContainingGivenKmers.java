f20fc0df7a96a53a2169b45cf2dbf199370aa3ad
Kiran V Garimella <kiran@well.ox.ac.uk>
2015-05-14 19:16:10 +0100

    Emit reads that contain a specified kmer
    Former-commit-id: 596b28478acc1206750a6b4f6d52fb1cea3f8e45

---
+++ public/java/src/uk/ac/ox/well/indiana/commands/gg/ReadsContainingGivenKmers.java
@@ -0,0 +1,49 @@
+package uk.ac.ox.well.indiana.commands.gg;
+
+import htsjdk.samtools.SAMFileReader;
+import htsjdk.samtools.SAMFileWriter;
+import htsjdk.samtools.SAMFileWriterFactory;
+import htsjdk.samtools.SAMRecord;
+import uk.ac.ox.well.indiana.commands.Module;
+import uk.ac.ox.well.indiana.utils.arguments.Argument;
+import uk.ac.ox.well.indiana.utils.arguments.Output;
+import uk.ac.ox.well.indiana.utils.io.cortex.graph.CortexKmer;
+
+import java.io.File;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.HashSet;
+
+public class ReadsContainingGivenKmers extends Module {
+    @Argument(fullName="bam", shortName="b", doc="BAM file")
+    public SAMFileReader BAM;
+
+    @Argument(fullName="listOfKmers", shortName="l", doc="List of kmers")
+    public HashSet<CortexKmer> KMER_LIST;
+
+    @Output
+    public File out;
+
+    @Override
+    public void execute() {
+        int kmerSize = KMER_LIST.iterator().next().length();
+
+        SAMFileWriter sfw = new SAMFileWriterFactory()
+                .setCreateIndex(true)
+                .makeBAMWriter(BAM.getFileHeader(), true, out);
+
+        for (SAMRecord read : BAM) {
+            String readSeq = read.getReadString();
+
+            for (int i = 0; i <= readSeq.length() - kmerSize; i++) {
+                CortexKmer ck = new CortexKmer(readSeq.substring(i, i + kmerSize));
+
+                if (KMER_LIST.contains(ck)) {
+                    sfw.addAlignment(read);
+                }
+            }
+        }
+
+        sfw.close();
+    }
+}
