4da21a23a0c43d45a637ca89705f77134164ec34
Kiran V Garimella <kiran@well.ox.ac.uk>
2015-06-01 12:01:49 +0100

    Print a fasta file of the number of novel stretches

---
+++ public/java/src/uk/ac/ox/well/indiana/commands/gg/PrintNovelStretches.java
@@ -0,0 +1,62 @@
+package uk.ac.ox.well.indiana.commands.gg;
+
+import uk.ac.ox.well.indiana.commands.Module;
+import uk.ac.ox.well.indiana.utils.arguments.Argument;
+import uk.ac.ox.well.indiana.utils.arguments.Output;
+import uk.ac.ox.well.indiana.utils.io.cortex.graph.CortexGraph;
+import uk.ac.ox.well.indiana.utils.io.cortex.graph.CortexKmer;
+import uk.ac.ox.well.indiana.utils.io.cortex.graph.CortexRecord;
+import uk.ac.ox.well.indiana.utils.sequence.CortexUtils;
+
+import java.io.PrintStream;
+import java.util.HashMap;
+import java.util.Map;
+
+public class PrintNovelStretches extends Module {
+    @Argument(fullName="graph", shortName="g", doc="Graph")
+    public CortexGraph GRAPH;
+
+    @Argument(fullName="novelGraph", shortName="n", doc="Graph of novel kmers")
+    public CortexGraph NOVEL;
+
+    @Output
+    public PrintStream out;
+
+    @Override
+    public void execute() {
+        Map<CortexKmer, Boolean> novelKmers = new HashMap<CortexKmer, Boolean>();
+        for (CortexRecord cr : NOVEL) {
+            novelKmers.put(new CortexKmer(cr.getKmerAsString()), true);
+        }
+
+        int totalNovelKmersUsed = 0;
+        int stretchNum = 0;
+        for (CortexKmer novelKmer : novelKmers.keySet()) {
+            if (novelKmers.get(novelKmer)) {
+                int novelKmersUsed = 0;
+
+                String stretch = CortexUtils.getSeededStretch(GRAPH, novelKmer.getKmerAsString(), 0);
+                for (int i = 0; i <= stretch.length() - novelKmer.length(); i++) {
+                    CortexKmer ck = new CortexKmer(stretch.substring(i, i + novelKmer.length()));
+
+                    if (novelKmers.containsKey(ck) && novelKmers.get(ck)) {
+                        totalNovelKmersUsed++;
+                        novelKmersUsed++;
+                        novelKmers.put(ck, false);
+                    }
+                }
+
+                log.info("Found {} bp stretch, used {}/{} novel kmers, {}/{} remain",
+                        stretch.length(),
+                        novelKmersUsed, novelKmers.size(),
+                        totalNovelKmersUsed, novelKmers.size()
+                );
+
+                out.println(">stretch_" + stretchNum + ".length_" + stretch.length());
+                out.println(stretch);
+
+                stretchNum++;
+            }
+        }
+    }
+}
