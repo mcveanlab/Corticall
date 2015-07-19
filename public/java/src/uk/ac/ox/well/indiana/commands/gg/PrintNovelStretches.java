adf6f1028503f9d7b480c0762ceeb16dd96164ce
Kiran V Garimella <kiran@well.ox.ac.uk>
2015-07-19 17:26:35 +0100

    Print the variant info that corresponds to the novel stretch

--- public/java/src/uk/ac/ox/well/indiana/commands/gg/PrintNovelStretches.java
+++ public/java/src/uk/ac/ox/well/indiana/commands/gg/PrintNovelStretches.java
@@ -6,11 +6,15 @@ import uk.ac.ox.well.indiana.utils.arguments.Output;
 import uk.ac.ox.well.indiana.utils.io.cortex.graph.CortexGraph;
 import uk.ac.ox.well.indiana.utils.io.cortex.graph.CortexKmer;
 import uk.ac.ox.well.indiana.utils.io.cortex.graph.CortexRecord;
+import uk.ac.ox.well.indiana.utils.io.table.TableReader;
 import uk.ac.ox.well.indiana.utils.sequence.CortexUtils;
 
+import java.io.File;
 import java.io.PrintStream;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 public class PrintNovelStretches extends Module {
     @Argument(fullName="graph", shortName="g", doc="Graph")
@@ -19,11 +23,84 @@ public class PrintNovelStretches extends Module {
     @Argument(fullName="novelGraph", shortName="n", doc="Graph of novel kmers")
     public CortexGraph NOVEL;
 
+    @Argument(fullName="novelKmerMap", shortName="m", doc="Novel kmer map")
+    public File NOVEL_KMER_MAP;
+
     @Output
     public PrintStream out;
 
+    private class VariantInfo {
+        public String variantId;
+        public String vclass;
+        public String vchr;
+        public int vstart;
+        public int vstop;
+
+        @Override
+        public String toString() {
+            return "VariantInfo{" +
+                    "variantId='" + variantId + '\'' +
+                    ", vclass='" + vclass + '\'' +
+                    ", vchr='" + vchr + '\'' +
+                    ", vstart=" + vstart +
+                    ", vstop=" + vstop +
+                    '}';
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            VariantInfo that = (VariantInfo) o;
+
+            if (vstart != that.vstart) return false;
+            if (vstop != that.vstop) return false;
+            if (variantId != null ? !variantId.equals(that.variantId) : that.variantId != null) return false;
+            if (vclass != null ? !vclass.equals(that.vclass) : that.vclass != null) return false;
+            return !(vchr != null ? !vchr.equals(that.vchr) : that.vchr != null);
+
+        }
+
+        @Override
+        public int hashCode() {
+            int result = variantId != null ? variantId.hashCode() : 0;
+            result = 31 * result + (vclass != null ? vclass.hashCode() : 0);
+            result = 31 * result + (vchr != null ? vchr.hashCode() : 0);
+            result = 31 * result + vstart;
+            result = 31 * result + vstop;
+            return result;
+        }
+    }
+
+    private Map<CortexKmer, VariantInfo> loadNovelKmerMap() {
+        Map<CortexKmer, VariantInfo> vis = new HashMap<CortexKmer, VariantInfo>();
+
+        TableReader tr = new TableReader(NOVEL_KMER_MAP);
+
+        for (Map<String, String> te : tr) {
+            VariantInfo vi = new VariantInfo();
+
+            if (!te.get("vstart").equals("NA")) {
+                vi.variantId = te.get("variantId");
+                vi.vclass = te.get("vclass");
+                vi.vchr = te.get("vchr");
+                vi.vstart = Integer.valueOf(te.get("vstart"));
+                vi.vstop = Integer.valueOf(te.get("vstop"));
+
+                CortexKmer kmer = new CortexKmer(te.get("kmer"));
+
+                vis.put(kmer, vi);
+            }
+        }
+
+        return vis;
+    }
+
     @Override
     public void execute() {
+        Map<CortexKmer, VariantInfo> vis = loadNovelKmerMap();
+
         Map<CortexKmer, Boolean> novelKmers = new HashMap<CortexKmer, Boolean>();
         for (CortexRecord cr : NOVEL) {
             novelKmers.put(new CortexKmer(cr.getKmerAsString()), true);
@@ -36,6 +113,8 @@ public class PrintNovelStretches extends Module {
                 int novelKmersUsed = 0;
 
                 String stretch = CortexUtils.getSeededStretch(GRAPH, novelKmer.getKmerAsString(), 0);
+                Set<VariantInfo> vs = new HashSet<VariantInfo>();
+
                 for (int i = 0; i <= stretch.length() - novelKmer.length(); i++) {
                     CortexKmer ck = new CortexKmer(stretch.substring(i, i + novelKmer.length()));
 
@@ -43,6 +122,10 @@ public class PrintNovelStretches extends Module {
                         totalNovelKmersUsed++;
                         novelKmersUsed++;
                         novelKmers.put(ck, false);
+                    }
+
+                    if (vis.containsKey(ck)) {
+                        vs.add(vis.get(ck));
                     }
                 }
 
@@ -54,6 +137,11 @@ public class PrintNovelStretches extends Module {
 
                 out.println(">stretch_" + stretchNum + ".length_" + stretch.length());
                 out.println(stretch);
+
+                log.info(">stretch_{}.length_{}", stretchNum, stretch.length());
+                for (VariantInfo vi : vs) {
+                    log.info("\t{}", vi);
+                }
 
                 stretchNum++;
             }
