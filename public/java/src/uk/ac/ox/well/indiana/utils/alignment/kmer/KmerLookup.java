551f822dc1e4a7701964aa8dc0c44c925a5a4085
Kiran V Garimella <kiran@well.ox.ac.uk>
2015-07-31 13:45:44 +0100

    Minor refactorings
    Former-commit-id: 02603ba45f47ef724dd82fd5a5f6482f6237c703

--- public/java/src/uk/ac/ox/well/indiana/utils/alignment/kmer/KmerLookup.java
+++ public/java/src/uk/ac/ox/well/indiana/utils/alignment/kmer/KmerLookup.java
@@ -1,6 +1,8 @@
 package uk.ac.ox.well.indiana.utils.alignment.kmer;
 
+import htsjdk.samtools.SAMRecord;
 import htsjdk.samtools.util.Interval;
+import uk.ac.ox.well.indiana.utils.containers.ContainerUtils;
 
 import java.io.File;
 import java.util.*;
@@ -43,5 +45,29 @@ public class KmerLookup {
         }
 
         return allIntervals;
+    }
+
+    public String findSimplified(String s) {
+        List<Set<Interval>> allIntervals = find(s);
+        StringBuilder simplified = new StringBuilder();
+
+        /*
+        Map<String, Integer> chrCounts = new HashMap<String, Integer>();
+        for (Set<Interval> intervals : allIntervals) {
+            Map<String, Integer> cc = new HashMap<String, Integer>();
+
+            for (Interval interval : intervals) {
+                ContainerUtils.increment(cc, interval.getSequence());
+            }
+
+            ContainerUtils.increment(chrCounts, ContainerUtils.mostCommonKey(cc));
+        }
+        */
+
+        for (Set<Interval> intervals : allIntervals) {
+            simplified.append(intervals.size() == 0 ? "0" : "1");
+        }
+
+        return simplified.toString();
     }
 }
