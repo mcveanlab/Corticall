4ccb229b1f1b66df12c60470e66b7b2c9e001708
Kiran V Garimella <kiran@well.ox.ac.uk>
2016-01-15 14:49:38 +0000

    Slice a reference
    Former-commit-id: 3b0f3126c771cfb5608da539a10ebf32adb3b4ac

---
+++ public/java/src/uk/ac/ox/well/indiana/commands/pacbio/SliceReference.java
@@ -0,0 +1,35 @@
+package uk.ac.ox.well.indiana.commands.pacbio;
+
+import htsjdk.samtools.reference.FastaSequenceFile;
+import htsjdk.samtools.reference.ReferenceSequence;
+import uk.ac.ox.well.indiana.commands.Module;
+import uk.ac.ox.well.indiana.utils.arguments.Argument;
+import uk.ac.ox.well.indiana.utils.arguments.Output;
+
+import java.io.PrintStream;
+
+public class SliceReference extends Module {
+    @Argument(fullName="ref", shortName="r", doc="Reference")
+    public FastaSequenceFile REF;
+
+    @Argument(fullName="length", shortName="l", doc="Length")
+    public Integer LENGTH = 100;
+
+    @Output
+    public PrintStream out;
+
+    @Override
+    public void execute() {
+        ReferenceSequence rseq;
+        while ((rseq = REF.nextSequence()) != null) {
+            String seq = new String(rseq.getBases());
+
+            for (int i = 0; i < rseq.length(); i += LENGTH) {
+                String slice = seq.substring(i, i + LENGTH);
+
+                out.println(">" + rseq.getName() + "." + i);
+                out.println(slice);
+            }
+        }
+    }
+}
