21d194908bbfec8a34bebf1f094f37fcd888d2d7
Kiran V Garimella <kiran@well.ox.ac.uk>
2016-01-24 23:13:11 +0000

    Stats on novel kmer classification
    Former-commit-id: 80a83db9b120f1eafd1a475e85fe0efb5d3e8f45

---
+++ public/java/src/uk/ac/ox/well/indiana/commands/evaluate/PacBioStatsNovelKmers.java
@@ -0,0 +1,55 @@
+package uk.ac.ox.well.indiana.commands.evaluate;
+
+import uk.ac.ox.well.indiana.commands.Module;
+import uk.ac.ox.well.indiana.utils.arguments.Argument;
+import uk.ac.ox.well.indiana.utils.io.cortex.graph.CortexGraph;
+import uk.ac.ox.well.indiana.utils.io.cortex.graph.CortexRecord;
+
+public class PacBioStatsNovelKmers extends Module {
+    @Argument(fullName="graph", shortName="g", doc="Graph")
+    public CortexGraph GRAPH;
+
+    @Override
+    public void execute() {
+        int tp = 0, tn = 0, fp = 0, fn = 0, un = 0;
+
+        for (CortexRecord cr : GRAPH) {
+            boolean calledNovel = cr.getCoverage(0) > 0;
+            boolean rejectedNovel = cr.getCoverage(1) > 0;
+            boolean presentInPacBio = cr.getCoverage(2) > 0;
+            boolean presentInChild = cr.getCoverage(3) > 0;
+            boolean presentInMom = cr.getCoverage(4) > 0;
+            boolean presentInDad = cr.getCoverage(5) > 0;
+            boolean presentInParent = presentInMom || presentInDad;
+
+            // Called a novel
+            if (calledNovel) {
+                // Correct? TP
+                if (presentInPacBio && presentInChild && !presentInParent) {
+                    tp++;
+
+                    log.info("tp: {}", cr);
+                } else {
+                    fp++;
+
+                    log.info("fp: {}", cr);
+                }
+            } else if (rejectedNovel) {
+                // Correct? TN
+                if (!presentInPacBio) {
+                    tn++;
+
+                    log.info("tn: {}", cr);
+                } else {
+                    fn++;
+
+                    log.info("fn: {}", cr);
+                }
+            } else {
+                un++;
+
+                log.info("un: {}", cr);
+            }
+        }
+    }
+}
