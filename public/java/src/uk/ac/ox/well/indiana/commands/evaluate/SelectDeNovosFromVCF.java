83c5b68a87e1a5ad76edbdc06c0a33a28e68c9b0
Kiran V Garimella <kiran@well.ox.ac.uk>
2016-01-26 15:25:31 +0000

    Select de novo variants from a VCF

---
+++ public/java/src/uk/ac/ox/well/indiana/commands/evaluate/SelectDeNovosFromVCF.java
@@ -0,0 +1,36 @@
+package uk.ac.ox.well.indiana.commands.evaluate;
+
+import htsjdk.variant.variantcontext.VariantContext;
+import htsjdk.variant.vcf.VCFFileReader;
+import uk.ac.ox.well.indiana.commands.Module;
+import uk.ac.ox.well.indiana.utils.arguments.Argument;
+import uk.ac.ox.well.indiana.utils.arguments.Output;
+
+import java.io.PrintStream;
+
+public class SelectDeNovosFromVCF extends Module {
+    @Argument(fullName="vcf", shortName="v", doc="VCF")
+    public VCFFileReader VCF;
+
+    @Argument(fullName="child", shortName="c", doc="Child")
+    public String CHILD;
+
+    @Argument(fullName="mother", shortName="m", doc="Mother")
+    public String MOTHER;
+
+    @Argument(fullName="father", shortName="f", doc="Father")
+    public String FATHER;
+
+    @Output
+    public PrintStream out;
+
+    @Override
+    public void execute() {
+        for (VariantContext vc : VCF) {
+            if (!vc.getGenotype(CHILD).getType().equals(vc.getGenotype(MOTHER).getType()) &&
+                !vc.getGenotype(CHILD).getType().equals(vc.getGenotype(FATHER).getType())) {
+                out.println(vc);
+            }
+        }
+    }
+}
