c3129dc62bcc6d1699e493ff596dd060ef8495c6
Kiran V Garimella <kiran@well.ox.ac.uk>
2016-10-17 15:53:00 +0100

    Write novel kmers as a graph rather than a database
    Former-commit-id: c28d8bd16295c46016d409a1dce917b59f05883b

--- public/java/src/uk/ac/ox/well/indiana/commands/playground/kmerdb/BuildDB.java
+++ public/java/src/uk/ac/ox/well/indiana/commands/playground/kmerdb/BuildDB.java
@@ -1,21 +1,18 @@
 package uk.ac.ox.well.indiana.commands.playground.kmerdb;
 
+import com.google.common.base.Joiner;
 import org.mapdb.*;
 import uk.ac.ox.well.indiana.commands.Module;
 import uk.ac.ox.well.indiana.utils.arguments.Argument;
 import uk.ac.ox.well.indiana.utils.arguments.Output;
 import uk.ac.ox.well.indiana.utils.io.cortex.collection.CortexCollection;
-import uk.ac.ox.well.indiana.utils.io.cortex.graph.CortexGraph;
-import uk.ac.ox.well.indiana.utils.io.cortex.graph.CortexRecord;
+import uk.ac.ox.well.indiana.utils.io.cortex.graph.*;
 import uk.ac.ox.well.indiana.utils.progress.ProgressMeter;
 import uk.ac.ox.well.indiana.utils.progress.ProgressMeterFactory;
 import uk.ac.ox.well.indiana.utils.sequence.CortexUtils;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class BuildDB extends Module {
     @Argument(fullName="dirtyGraph", shortName="d", doc="Dirty graph")
@@ -23,6 +20,9 @@ public class BuildDB extends Module {
 
     @Argument(fullName="childColor", shortName="cc", doc="Child color")
     public Integer CHILD_COLOR = 0;
+
+    @Argument(fullName="parentColor", shortName="pc", doc="Parent color")
+    public HashSet<Integer> PARENT_COLORS = new HashSet<>();
 
     @Argument(fullName="coverageLowerLimit", shortName="l", doc="Coverage lower limit")
     public Integer COVERAGE_LOWER_LIMIT = 1;
@@ -32,10 +32,42 @@ public class BuildDB extends Module {
 
     @Override
     public void execute() {
-        List<long[]> binaryKmers = new ArrayList<>();
+        if (PARENT_COLORS.isEmpty()) {
+            for (int c = 0; c < DIRTY.getNumColors(); c++) {
+                if (c != CHILD_COLOR) {
+                    PARENT_COLORS.add(c);
+                }
+            }
+        }
+
+        log.info("Colors: (child) {} (parents) {}", CHILD_COLOR, Joiner.on(",").join(PARENT_COLORS));
+
+        CortexHeader ch = new CortexHeader();
+        ch.setVersion(6);
+        ch.setNumColors(DIRTY.getNumColors());
+        ch.setKmerSize(DIRTY.getKmerSize());
+        ch.setKmerBits(DIRTY.getKmerBits());
+
+        for (int c = 0; c < DIRTY.getNumColors(); c++) {
+            CortexColor cc = new CortexColor();
+            cc.setCleanedAgainstGraph(false);
+            cc.setCleanedAgainstGraphName("");
+            cc.setErrorRate(0);
+            cc.setLowCovgKmersRemoved(false);
+            cc.setLowCovgSupernodesRemoved(false);
+            cc.setSampleName("color" + c);
+            cc.setTipClippingApplied(false);
+            cc.setTotalSequence(0);
+
+            ch.addColor(cc);
+        }
+
+        CortexGraphWriter cgw = new CortexGraphWriter(out);
+        cgw.setHeader(ch);
+
+        //List<long[]> binaryKmers = new ArrayList<>();
 
         ProgressMeter pm = new ProgressMeterFactory()
-                //.maxRecord(DIRTY.getNumRecords())
                 .header("Processing graph...")
                 .message("records processed")
                 .updateRecord(DIRTY.getGraph(0).getNumRecords() / 10)
@@ -43,8 +75,10 @@ public class BuildDB extends Module {
 
         long numNovelRecords = 0;
         for (CortexRecord cr : DIRTY) {
-            if (CortexUtils.isNovelKmer(cr, CHILD_COLOR) && cr.getCoverage(CHILD_COLOR) > COVERAGE_LOWER_LIMIT) {
-                binaryKmers.add(cr.getBinaryKmer());
+            if (CortexUtils.isNovelKmer(cr, CHILD_COLOR, PARENT_COLORS) && cr.getCoverage(CHILD_COLOR) > COVERAGE_LOWER_LIMIT) {
+                //binaryKmers.add(cr.getBinaryKmer());
+
+                cgw.addRecord(cr);
 
                 numNovelRecords++;
             }
@@ -54,6 +88,9 @@ public class BuildDB extends Module {
 
         log.info("  {} total novel records", numNovelRecords);
 
+        cgw.close();
+
+        /*
         if (out.exists()) {
             out.delete();
         }
@@ -63,8 +100,11 @@ public class BuildDB extends Module {
                 .fileMmapEnable()
                 .make();
 
-        Atomic.Integer aKmerSize = db.atomicInteger("kmerSize", DIRTY.getKmerSize()).create();
-        Atomic.Integer aKmerBits = db.atomicInteger("kmerBits", DIRTY.getKmerBits()).create();
+        //Atomic.Integer aKmerSize = db.atomicInteger("kmerSize", DIRTY.getKmerSize()).create();
+        //Atomic.Integer aKmerBits = db.atomicInteger("kmerBits", DIRTY.getKmerBits()).create();
+
+        db.atomicInteger("kmerSize", DIRTY.getKmerSize()).create();
+        db.atomicInteger("kmerBits", DIRTY.getKmerBits()).create();
 
         HTreeMap<long[], Map<String, Object>> nkdb = db.hashMap("novelKmers")
                 .keySerializer(Serializer.LONG_ARRAY)
@@ -86,5 +126,6 @@ public class BuildDB extends Module {
         db.commit();
         nkdb.close();
         db.close();
+        */
     }
 }
