b757c4503870e72de50ef9f7226064efb9baf67e
Kiran V Garimella <kiran@well.ox.ac.uk>
2017-03-24 00:22:43 +0000

    Try only building contigs with child data first

---
+++ public/java/src/uk/ac/ox/well/indiana/utils/traversal/NovelKmerAggregationStopper.java
@@ -0,0 +1,26 @@
+package uk.ac.ox.well.indiana.utils.traversal;
+
+import org.jgrapht.DirectedGraph;
+import uk.ac.ox.well.indiana.utils.io.cortex.graph.CortexRecord;
+
+import java.util.Set;
+
+/**
+ * Created by kiran on 24/03/2017.
+ */
+public class NovelKmerAggregationStopper extends AbstractTraversalStopper<AnnotatedVertex, AnnotatedEdge> {
+    @Override
+    public boolean hasTraversalSucceeded(CortexRecord cr, DirectedGraph<AnnotatedVertex, AnnotatedEdge> g, int junctions, int size, int edges, Set<Integer> childColors, Set<Integer> parentColors) {
+        return cr.getInDegree(0) == 0 || cr.getOutDegree(0) == 0;
+    }
+
+    @Override
+    public boolean hasTraversalFailed(CortexRecord cr, DirectedGraph<AnnotatedVertex, AnnotatedEdge> g, int junctions, int size, int edges, Set<Integer> childColors, Set<Integer> parentColors) {
+        return false;
+    }
+
+    @Override
+    public int maxJunctionsAllowed() {
+        return 0;
+    }
+}
