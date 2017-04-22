5909e015fee599707872a22b170e8e60dc5f93ec
Kiran V Garimella <kiran@well.ox.ac.uk>
2017-04-22 20:37:01 +0100

    More annotations
    Former-commit-id: a1c88cb4b537cdd7b5020976a7df83949795fbda

--- public/java/src/uk/ac/ox/well/indiana/commands/playground/caller/AnnotateROIs.java
+++ public/java/src/uk/ac/ox/well/indiana/commands/playground/caller/AnnotateROIs.java
@@ -12,32 +12,44 @@ import uk.ac.ox.well.indiana.utils.io.cortex.graph.CortexRecord;
 
 import java.io.File;
 import java.io.PrintStream;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class AnnotateROIs extends Module {
+    @Argument(fullName="combined", shortName="com", doc="Graph (combined)")
+    public CortexGraph COMBINED;
+
     @Argument(fullName="graph", shortName="g", doc="Graph")
     public CortexGraph GRAPH;
 
     @Argument(fullName="roi", shortName="r", doc="ROI")
     public CortexGraph ROI;
 
-    @Argument(fullName="contam", shortName="c", doc="Contam")
+    @Argument(fullName="contam", shortName="con", doc="Contam")
     public CortexGraph CONTAM;
 
     @Argument(fullName="bam", shortName="b", doc="BAM")
     public File BAM_FILE;
+
+    @Argument(fullName="child", shortName="c", doc="Child")
+    public String CHILD;
+
+    @Argument(fullName="parents", shortName="p", doc="Parents")
+    public ArrayList<String> PARENTS;
 
     @Output
     public PrintStream out;
 
     @Override
     public void execute() {
+        int childColor = GRAPH.getColorForSampleName(CHILD);
+        Set<Integer> parentColors = new HashSet<>();
+        for (String parent : PARENTS) {
+            parentColors.add(GRAPH.getColorForSampleName(parent));
+        }
+
         Map<CortexKmer, Map<String, Object>> anns = new HashMap<>();
 
-        KmerIndex ki = new KmerIndex(BAM_FILE, GRAPH.getKmerSize(), false);
+        KmerIndex ki = new KmerIndex(BAM_FILE, ROI.getKmerSize(), false);
         SamReader sr = SamReaderFactory.make()
                 .setOption(SamReaderFactory.Option.INCLUDE_SOURCE_IN_RECORDS, true)
                 .setOption(SamReaderFactory.Option.CACHE_FILE_BASED_INDEXES, true)
@@ -48,8 +60,47 @@ public class AnnotateROIs extends Module {
         graphs.put("roi", ROI);
         graphs.put("contam", CONTAM);
 
+        Map<String, Set<CortexRecord>> recset = new HashMap<>();
         for (String label : graphs.keySet()) {
+            recset.put(label, new HashSet<>());
+
             for (CortexRecord cr : graphs.get(label)) {
+                recset.get(label).add(cr);
+            }
+        }
+
+        recset.put("dirty", new HashSet<>());
+
+        for (CortexRecord cr : COMBINED) {
+            if (cr.getCoverage(0) > 0 && cr.getCoverage(1) == 0) {
+                recset.get("dirty").add(cr);
+            }
+        }
+
+        recset.put("clean", new HashSet<>());
+        for (CortexRecord cr : GRAPH) {
+            if (recset.get("clean").size() > 100000) {
+                break;
+            }
+
+            if (cr.getInDegree(childColor) <= 1 && cr.getOutDegree(childColor) <= 1 && cr.getCoverage(childColor) > 0) {
+                boolean isInAllSamples = true;
+
+                for (int c = 0; c < cr.getNumColors(); c++) {
+                    if (cr.getCoverage(c) == 0) {
+                        isInAllSamples = false;
+                        break;
+                    }
+                }
+
+                if (isInAllSamples) {
+                    recset.get("clean").add(cr);
+                }
+            }
+        }
+
+        for (String label : recset.keySet()) {
+            for (CortexRecord cr : recset.get(label)) {
                 //Map<String, Object> ann = new HashMap<>();
                 //ann.put("")
 
@@ -66,13 +117,13 @@ public class AnnotateROIs extends Module {
                     while (recs.hasNext()) {
                         SAMRecord rec = recs.next();
 
-                        for (int i = 0; i <= rec.getReadLength() - GRAPH.getKmerSize(); i++) {
-                            CortexKmer ck = new CortexKmer(rec.getReadString().substring(i, i + GRAPH.getKmerSize()));
+                        for (int i = 0; i <= rec.getReadLength() - ROI.getKmerSize(); i++) {
+                            CortexKmer ck = new CortexKmer(rec.getReadString().substring(i, i + ROI.getKmerSize()));
 
                             if (cr.getCortexKmer().equals(ck)) {
                                 byte minQual = 40;
 
-                                for (int j = i; j < i + GRAPH.getKmerSize(); j++) {
+                                for (int j = i; j < i + ROI.getKmerSize(); j++) {
                                     byte qual = rec.getBaseQualities()[j];
 
                                     if (qual < minQual) {
