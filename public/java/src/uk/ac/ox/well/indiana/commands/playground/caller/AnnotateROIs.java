7b7f27779d274fa810eeb4786817ebe00f550363
Kiran V Garimella <kiran@well.ox.ac.uk>
2017-04-22 17:59:18 +0100

    Label kmers

--- public/java/src/uk/ac/ox/well/indiana/commands/playground/caller/AnnotateROIs.java
+++ public/java/src/uk/ac/ox/well/indiana/commands/playground/caller/AnnotateROIs.java
@@ -38,51 +38,62 @@ public class AnnotateROIs extends Module {
         Map<CortexKmer, Map<String, Object>> anns = new HashMap<>();
 
         KmerIndex ki = new KmerIndex(BAM_FILE, GRAPH.getKmerSize(), false);
-        SamReader sr = SamReaderFactory.makeDefault().open(BAM_FILE);
+        SamReader sr = SamReaderFactory
+                .makeDefault()
+                .setOption(SamReaderFactory.Option.INCLUDE_SOURCE_IN_RECORDS, true)
+                .setOption(SamReaderFactory.Option.CACHE_FILE_BASED_INDEXES, true)
+                .validationStringency(ValidationStringency.STRICT)
+                .open(BAM_FILE);
 
-        for (CortexRecord cr : ROI) {
-            //Map<String, Object> ann = new HashMap<>();
-            //ann.put("")
+        Map<String, CortexGraph> graphs = new HashMap<>();
+        graphs.put("roi", ROI);
+        graphs.put("contam", CONTAM);
 
-            List<long[]> chunks = ki.find(cr.getKmerAsBytes());
+        for (String label : graphs.keySet()) {
+            for (CortexRecord cr : graphs.get(label)) {
+                //Map<String, Object> ann = new HashMap<>();
+                //ann.put("")
 
-            List<Byte> quals = new ArrayList<>();
-            int qualMin = 0;
+                List<long[]> chunks = ki.find(cr.getKmerAsBytes());
 
-            for (long[] c : chunks) {
-                SAMFileSpan sfs = new BAMFileSpan(new Chunk(c[0], c[1]));
+                List<Byte> quals = new ArrayList<>();
+                int qualMin = 0;
 
-                SAMRecordIterator recs = sr.indexing().iterator(sfs);
+                for (long[] c : chunks) {
+                    SAMFileSpan sfs = new BAMFileSpan(new Chunk(c[0], c[1]));
 
-                while (recs.hasNext()) {
-                    SAMRecord rec = recs.next();
+                    SAMRecordIterator recs = sr.indexing().iterator(sfs);
 
-                    //log.info("    {}: {} {}", cr, new Chunk(c[0], c[1]), rec.getSAMString());
+                    while (recs.hasNext()) {
+                        SAMRecord rec = recs.next();
 
-                    for (int i = 0; i <= rec.getReadLength() - GRAPH.getKmerSize(); i++) {
-                        CortexKmer ck = new CortexKmer(rec.getReadString().substring(i, i + GRAPH.getKmerSize()));
+                        for (int i = 0; i <= rec.getReadLength() - GRAPH.getKmerSize(); i++) {
+                            CortexKmer ck = new CortexKmer(rec.getReadString().substring(i, i + GRAPH.getKmerSize()));
 
-                        if (cr.getCortexKmer().equals(ck)) {
-                            byte minQual = 40;
+                            if (cr.getCortexKmer().equals(ck)) {
+                                byte minQual = 40;
 
-                            for (int j = i; j < i + GRAPH.getKmerSize(); j++) {
-                                byte qual = rec.getBaseQualities()[j];
+                                for (int j = i; j < i + GRAPH.getKmerSize(); j++) {
+                                    byte qual = rec.getBaseQualities()[j];
 
-                                if (qual < minQual) {
-                                    minQual = qual;
+                                    if (qual < minQual) {
+                                        minQual = qual;
+                                    }
                                 }
-                            }
 
-                            quals.add(minQual);
-                            qualMin += minQual;
+                                quals.add(minQual);
+                                qualMin += minQual;
+                            }
                         }
                     }
+
+                    recs.close();
                 }
 
-                recs.close();
-            }
+                //log.info("{} {} {}", cr, qualMin / quals.size(), Joiner.on(", ").join(quals));
 
-            log.info("{} {} {}", cr, qualMin / quals.size(), Joiner.on(", ").join(quals));
+                out.println(Joiner.on("\t").join(label, cr.getKmerAsString(), cr.getCoverage(0), qualMin / quals.size()));
+            }
         }
     }
 }
