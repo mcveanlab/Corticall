74ffa546763f0178940702e8cdfd459855675238
Kiran V Garimella <kiran@well.ox.ac.uk>
2017-04-26 09:50:58 +0100

    Find validated novel kmers
    Former-commit-id: 7e91ca07e9b79a1822f371d7eb7afabc94ffb7f9

---
+++ public/java/src/uk/ac/ox/well/indiana/commands/playground/filter/EmitValidatedNovelKmers.java
@@ -0,0 +1,75 @@
+package uk.ac.ox.well.indiana.commands.playground.filter;
+
+import org.jetbrains.annotations.NotNull;
+import uk.ac.ox.well.indiana.commands.Module;
+import uk.ac.ox.well.indiana.utils.arguments.Argument;
+import uk.ac.ox.well.indiana.utils.arguments.Output;
+import uk.ac.ox.well.indiana.utils.io.cortex.graph.*;
+
+import java.io.File;
+import java.io.PrintStream;
+
+/**
+ * Created by kiran on 26/04/2017.
+ */
+public class EmitValidatedNovelKmers extends Module {
+    @Argument(fullName="graph", shortName="g", doc="Graph")
+    public CortexGraph GRAPH;
+
+    @Output
+    public File out;
+
+    @Override
+    public void execute() {
+        /*
+        Colour 0: sample name: '803' (drafts)
+        Colour 1: sample name: 'PG0443-C' (clean)
+        Colour 2: sample name: 'PG0443-C' (tips)
+        Colour 3: sample name: 'PG0443-C' (raw)
+        Colour 4: sample name: 'GB4' (drafts)
+        Colour 5: sample name: 'PG0050-CX2' (clean)
+        Colour 6: sample name: 'PG0050-CX2' (tips)
+        Colour 7: sample name: 'PG0050-CX2' (raw)
+        Colour 8: sample name: '34F5' (drafts)
+        Colour 9: sample name: 'PG0476-C' (clean)
+        Colour 10: sample name: 'PG0476-C' (tips)
+        Colour 11: sample name: 'PG0476-C' (raw)
+        */
+
+        CortexGraphWriter cgw = new CortexGraphWriter(out);
+        cgw.setHeader(GRAPH.getHeader());
+        //cgw.setHeader(makeCortexHeader());
+
+        for (CortexRecord cr : GRAPH) {
+            if (cr.getCoverage(0) == 0 && cr.getCoverage(1) == 0 && cr.getCoverage(2) == 0 && cr.getCoverage(3) == 0 &&
+                cr.getCoverage(4) == 0 && cr.getCoverage(5) == 0 && cr.getCoverage(6) == 0 && cr.getCoverage(7) == 0 &&
+                cr.getCoverage(8)  > 0 && cr.getCoverage(9)  > 0 && cr.getCoverage(10) > 0 && cr.getCoverage(11) > 0) {
+
+                cgw.addRecord(cr);
+            }
+        }
+    }
+
+    @NotNull
+    private CortexHeader makeCortexHeader() {
+        CortexHeader ch = new CortexHeader();
+        ch.setVersion(6);
+        ch.setNumColors(1);
+        ch.setKmerSize(GRAPH.getKmerSize());
+        ch.setKmerBits(GRAPH.getKmerBits());
+
+        CortexColor cc = new CortexColor();
+        cc.setCleanedAgainstGraph(false);
+        cc.setCleanedAgainstGraphName("");
+        cc.setErrorRate(0);
+        cc.setLowCovgKmersRemoved(false);
+        cc.setLowCovgSupernodesRemoved(false);
+        cc.setTipClippingApplied(false);
+        cc.setTotalSequence(0);
+        cc.setSampleName(GRAPH.getSampleName(8));
+
+        ch.addColor(cc);
+
+        return ch;
+    }
+}
