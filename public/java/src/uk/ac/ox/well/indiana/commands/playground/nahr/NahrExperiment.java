1e05cd9966a0f7612825c7318a2917f88351b9d9
Kiran V Garimella <kiran@well.ox.ac.uk>
2017-05-08 04:05:26 +0100

    Experiments

--- public/java/src/uk/ac/ox/well/indiana/commands/playground/nahr/NahrExperiment.java
+++ public/java/src/uk/ac/ox/well/indiana/commands/playground/nahr/NahrExperiment.java
@@ -1,20 +1,13 @@
 package uk.ac.ox.well.indiana.commands.playground.nahr;
 
-import htsjdk.samtools.reference.FastaSequenceFile;
 import htsjdk.samtools.reference.IndexedFastaSequenceFile;
 import htsjdk.samtools.util.Interval;
-import org.jgrapht.DirectedGraph;
 import uk.ac.ox.well.indiana.commands.Module;
 import uk.ac.ox.well.indiana.utils.arguments.Argument;
 import uk.ac.ox.well.indiana.utils.io.cortex.graph.CortexGraph;
 import uk.ac.ox.well.indiana.utils.io.cortex.graph.CortexKmer;
 import uk.ac.ox.well.indiana.utils.io.cortex.graph.CortexRecord;
-import uk.ac.ox.well.indiana.utils.sequence.CortexUtils;
-import uk.ac.ox.well.indiana.utils.stoppingconditions.NahrStopper;
-import uk.ac.ox.well.indiana.utils.traversal.AnnotatedEdge;
-import uk.ac.ox.well.indiana.utils.traversal.AnnotatedVertex;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -22,8 +15,8 @@ import java.util.List;
  * Created by kiran on 08/05/2017.
  */
 public class NahrExperiment extends Module {
-    @Argument(fullName="genome", shortName="g", doc="Genome")
-    public IndexedFastaSequenceFile GENOME;
+    @Argument(fullName="reference", shortName="r", doc="Genome")
+    public IndexedFastaSequenceFile REF;
 
     @Argument(fullName="graph", shortName="g", doc="Graph")
     public CortexGraph GRAPH;
@@ -33,8 +26,8 @@ public class NahrExperiment extends Module {
         Interval it1 = new Interval("Pf3D7_01_v3", 29510, 37126);   // PF3D7_0100100
         Interval it2 = new Interval("Pf3D7_02_v3", 916352, 923648); // PF3D7_0223500
 
-        String var1 = GENOME.getSubsequenceAt(it1.getContig(), it1.getStart(), it1.getEnd()).getBaseString();
-        String var2 = GENOME.getSubsequenceAt(it2.getContig(), it2.getStart(), it2.getEnd()).getBaseString();
+        String var1 = REF.getSubsequenceAt(it1.getContig(), it1.getStart(), it1.getEnd()).getBaseString();
+        String var2 = REF.getSubsequenceAt(it2.getContig(), it2.getStart(), it2.getEnd()).getBaseString();
 
         int childColor = GRAPH.getColorForSampleName("PG0063-C");
         List<Integer> parentColors = GRAPH.getColorsForSampleNames(Arrays.asList("PG0051-C", "PG0052-C"));
