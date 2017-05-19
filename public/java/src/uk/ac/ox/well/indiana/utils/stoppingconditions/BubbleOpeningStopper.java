2bc306e8b24f1b7485aaa398f2430d750b2cd823
Kiran V Garimella <kiran@well.ox.ac.uk>
2017-05-19 21:53:38 +0100

    Added stopping conditions for bubble discovery

--- public/java/src/uk/ac/ox/well/indiana/utils/stoppingconditions/BubbleOpeningStopper.java
+++ public/java/src/uk/ac/ox/well/indiana/utils/stoppingconditions/BubbleOpeningStopper.java
@@ -16,26 +16,27 @@ import java.util.Set;
 public class BubbleOpeningStopper extends AbstractTraversalStopper<CortexVertex, CortexEdge>{
     @Override
     public boolean hasTraversalSucceeded(CortexRecord cr, DirectedGraph<CortexVertex, CortexEdge> g, int junctions, int size, int edges, int childColor, Set<Integer> parentColors) {
-        return (cr.getInDegree(childColor) > 1 || cr.getOutDegree(childColor) > 1);
+        return false;
     }
 
     @Override
     public boolean hasTraversalFailed(CortexRecord cr, DirectedGraph<CortexVertex, CortexEdge> g, int junctions, int size, int edges, int childColor, Set<Integer> parentColors) {
-        return (cr.getInDegree(childColor) == 0 || cr.getOutDegree(childColor) == 0);
+        return true;
     }
-
-    private boolean rejoined = false;
 
     @Override
     public boolean hasTraversalSucceeded(CortexVertex cv, boolean goForward, int traversalColor, Set<Integer> joiningColors, int currentTraversalDepth, int currentGraphSize, int numAdjacentEdges, boolean childrenAlreadyTraversed, DirectedGraph<CortexVertex, CortexEdge> previousGraph, CortexGraph rois) {
         boolean hasJoined = false;
 
+        for (int c : joiningColors) {
+            hasJoined |= (cv.getCr().getCoverage(c) > 0);
+        }
 
-        return false;
+        return hasJoined;
     }
 
     @Override
     public boolean hasTraversalFailed(CortexVertex cv, boolean goForward, int traversalColor, Set<Integer> joiningColors, int currentTraversalDepth, int currentGraphSize, int numAdjacentEdges, boolean childrenAlreadyTraversed, DirectedGraph<CortexVertex, CortexEdge> previousGraph, CortexGraph rois) {
-        return false;
+        return currentGraphSize > 1000 || currentTraversalDepth >= 5 || numAdjacentEdges == 0;
     }
 }
