7e3c9d842e90eee61f6cbdfd7d85a155d7c5edee
Kiran V Garimella <kiran@well.ox.ac.uk>
2017-05-30 03:27:23 +0100

    New debugging tool
    Former-commit-id: 2ee9e52e996a62079fd4316bf8e4ed4b0c94e682

--- public/java/src/uk/ac/ox/well/indiana/utils/stoppingconditions/ExplorationStopper.java
+++ public/java/src/uk/ac/ox/well/indiana/utils/stoppingconditions/ExplorationStopper.java
@@ -11,6 +11,9 @@ import uk.ac.ox.well.indiana.utils.traversal.CortexVertex;
 import java.util.Set;
 
 public class ExplorationStopper extends AbstractTraversalStopper<CortexVertex, CortexEdge> {
+    private boolean novelKmerFound = false;
+    private int distanceFromLastNovelKmer = -1;
+
     @Override
     public boolean hasTraversalSucceeded(CortexRecord cr, DirectedGraph<CortexVertex, CortexEdge> g, int junctions, int size, int edges, int childColor, Set<Integer> parentColors) {
         return false;
@@ -23,11 +26,27 @@ public class ExplorationStopper extends AbstractTraversalStopper<CortexVertex, C
 
     @Override
     public boolean hasTraversalSucceeded(CortexVertex cv, boolean goForward, int traversalColor, Set<Integer> joiningColors, int currentTraversalDepth, int currentGraphSize, int numAdjacentEdges, boolean childrenAlreadyTraversed, DirectedGraph<CortexVertex, CortexEdge> previousGraph, CortexGraph rois) {
-        return false;
+        boolean childHasCoverage = cv.getCr().getCoverage(traversalColor) > 0;
+        boolean parentHasCoverage = false;
+
+        for (int c : joiningColors) {
+            parentHasCoverage |= cv.getCr().getCoverage(c) > 0;
+        }
+
+        if (novelKmerFound) {
+            distanceFromLastNovelKmer++;
+        }
+
+        if (childHasCoverage && !parentHasCoverage) {
+            novelKmerFound = true;
+            distanceFromLastNovelKmer = 0;
+        }
+
+        return novelKmerFound && (distanceFromLastNovelKmer > 100 || childrenAlreadyTraversed);
     }
 
     @Override
     public boolean hasTraversalFailed(CortexVertex cv, boolean goForward, int traversalColor, Set<Integer> joiningColors, int currentTraversalDepth, int currentGraphSize, int numAdjacentEdges, boolean childrenAlreadyTraversed, DirectedGraph<CortexVertex, CortexEdge> previousGraph, CortexGraph rois) {
-        return false;
+        return !novelKmerFound && (currentGraphSize > 200 || numAdjacentEdges == 0 || currentTraversalDepth > 3);
     }
 }
