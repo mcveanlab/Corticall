38ea2c922180320cbaf7f4b36e34844b7fe6113e
Kiran V Garimella <kiran@well.ox.ac.uk>
2017-06-01 14:28:48 +0100

    Debugging sequencing error removal

--- public/java/src/uk/ac/ox/well/indiana/utils/visualizer/handlers/SubGraphListener.java
+++ public/java/src/uk/ac/ox/well/indiana/utils/visualizer/handlers/SubGraphListener.java
@@ -3,16 +3,19 @@ package uk.ac.ox.well.indiana.utils.visualizer.handlers;
 import com.google.api.client.http.HttpStatusCodes;
 import com.sun.net.httpserver.HttpExchange;
 import org.apache.commons.math3.util.Pair;
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Map;
+import java.util.TreeMap;
 
 /**
  * Created by kiran on 29/05/2017.
  */
 public class SubGraphListener extends BaseHandler {
-    private String preLoadedGraph = "";
+    private Map<String, JSONObject> preLoadedGraphs = new TreeMap<>();
 
     @Override
     public Pair<Integer, String> respond(HttpExchange httpExchange) throws IOException {
@@ -21,11 +24,15 @@ public class SubGraphListener extends BaseHandler {
 
             new BufferedReader(new InputStreamReader(httpExchange.getRequestBody()))
                     .lines()
-                    .forEach((String s) -> sb.append(s + "\n"));
+                    .forEach((String s) -> sb.append(s).append("\n"));
 
-            preLoadedGraph = sb.toString();
+            JSONObject jo = new JSONObject(sb.toString());
+            preLoadedGraphs.put(jo.getString("name"), jo);
         }
 
-        return new Pair<>(HttpStatusCodes.STATUS_CODE_OK, preLoadedGraph);
+        JSONObject graphNames = new JSONObject();
+        graphNames.put("names", preLoadedGraphs.keySet());
+
+        return new Pair<>(HttpStatusCodes.STATUS_CODE_OK, graphNames.toString());
     }
 }
