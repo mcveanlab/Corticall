b08a56dcacf26ff01a9c5fc0a7796cddb632b3a3
Kiran V Garimella <kiran@well.ox.ac.uk>
2017-06-02 01:41:11 +0100

    Add display colors to graph after construction

--- public/java/src/uk/ac/ox/well/indiana/utils/visualizer/handlers/SubGraphListener.java
+++ public/java/src/uk/ac/ox/well/indiana/utils/visualizer/handlers/SubGraphListener.java
@@ -47,7 +47,7 @@ public class SubGraphListener extends BaseHandler {
             for (int i = 0; i <= seq.length() - kmerSize; i++) {
                 String kmer = seq.substring(i, i + kmerSize);
 
-                CortexVertex cv = new CortexVertex(kmer, null, 0);
+                CortexVertex cv = new CortexVertex(kmer, null);
                 g.addVertex(cv);
 
                 if (lv != null) {
