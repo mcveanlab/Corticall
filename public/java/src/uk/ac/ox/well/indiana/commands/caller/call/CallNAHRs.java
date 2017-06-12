bedca1437d0b21f3b04b7fc74529995b2d70bb38
Kiran V Garimella <kiran@well.ox.ac.uk>
2017-06-12 22:58:58 +0100

    Testing NAHR caller
    Former-commit-id: 4a11f82fe393953a8dd2d805191f3b2e85d08911

--- public/java/src/uk/ac/ox/well/indiana/commands/caller/call/CallNAHRs.java
+++ public/java/src/uk/ac/ox/well/indiana/commands/caller/call/CallNAHRs.java
@@ -73,108 +73,107 @@ public class CallNAHRs extends Module {
             used.put(rr.getCortexKmer(), false);
         }
 
-        /*
-        Map<String, IntervalTreeMap<Interval>> candidateLoci = new TreeMap<>();
+        String sk = "ACATGTGGTTCAGGAGAATGGGCTAAAGACAAATGCCGCTGTAAGGA";
 
-        for (CortexKmer rk : used.keySet()) {
-            if (!used.get(rk)) {
-                log.info("{}", rk);
-
-                DirectedWeightedPseudograph<CortexVertex, CortexEdge> g = e.dfs(rk.getKmerAsString());
-
-                TopologicalOrderIterator<CortexVertex, CortexEdge> toi = new TopologicalOrderIterator<>(g);
-
-                Map<String, Integer> refCount = new HashMap<>();
-                Map<String, Map<String, Integer>> chrCount = new HashMap<>();
-                for (String parent : LOOKUPS.keySet()) {
-                    chrCount.put(parent, new HashMap<>());
-                }
-                int numNovelStretches = 0;
-                boolean inNovelStretch = false;
-
-                while (toi.hasNext()) {
-                    CortexVertex cv = toi.next();
-
-                    for (String parent : LOOKUPS.keySet()) {
-                        Set<Interval> intervals = LOOKUPS.get(parent).findKmer(cv.getSk());
-
-                        if (intervals.size() == 1) {
-                            ContainerUtils.increment(refCount, parent);
-                            ContainerUtils.increment(chrCount.get(parent), intervals.iterator().next().getContig());
-
-                            Interval newInterval = intervals.iterator().next();
-
-                            if (!candidateLoci.containsKey(newInterval.getContig())) {
-                                candidateLoci.put(newInterval.getContig(), new IntervalTreeMap<>());
-                            }
-
-                            candidateLoci.get(newInterval.getContig()).put(newInterval, newInterval);
-                        }
-                    }
-
-                    if (used.containsKey(cv.getCk())) {
-                        if (!inNovelStretch) {
-                            numNovelStretches++;
-                            inNovelStretch = true;
-                        }
-
-                        used.put(cv.getCk(), true);
-                    } else {
-                        inNovelStretch = false;
-                    }
-                }
-
-                String mostFrequentBackground = mostFrequentBackground(refCount, 3);
-
-                log.info(" -- stats {} {} {} {}", numNovelStretches, refCount, mostFrequentBackground, chrCount);
-
-                if (mostFrequentBackground != null && numNovelStretches > 1 && chrCount.get(mostFrequentBackground).keySet().size() > 1) {
-                    toi = new TopologicalOrderIterator<>(g);
-                    while (toi.hasNext()) {
-                        CortexVertex cv = toi.next();
-
-                        Set<Interval> intervals = LOOKUPS.get(mostFrequentBackground).findKmer(cv.getSk());
-
-                        if (intervals.size() == 1) {
-                            log.info(" -- {} {} {} {}", mostFrequentBackground, used.containsKey(cv.getCk()), intervals, recordToString(cv.getCr(), childColor, parentColors));
-                        } else {
-                            log.info(" -- {} [{}] {} {}", mostFrequentBackground, used.containsKey(cv.getCk()), intervals.size(), recordToString(cv.getCr(), childColor, parentColors));
-                        }
-                    }
-                }
-
-                log.info("");
-            }
-        }
-
-        candidateLoci = mergeIntervals(candidateLoci, 1);
-        candidateLoci = mergeIntervals(candidateLoci, 10);
-        candidateLoci = mergeIntervals(candidateLoci, 100);
-        candidateLoci = mergeIntervals(candidateLoci, 1000);
-        candidateLoci = mergeIntervals(candidateLoci, 10000);
-        candidateLoci = mergeIntervals(candidateLoci, 20000);
-
-        for (String contig : candidateLoci.keySet()) {
-            for (Interval interval : candidateLoci.get(contig).keySet()) {
-                log.info("{}", interval);
-            }
-        }
-        */
-
-        Interval it = new Interval("Pf3D7_01_v3", 29500, 29546);
-        String qk = LOOKUPS.get("ref").findKmer(it);
-
-        reconstruct("ref", qk);
+        reconstruct("ref", sk);
     }
 
     private void reconstruct(String background, String sk) {
         log.info("Fwd:");
-        Pair<List<String>, List<Interval>> fwd = reconstruct("ref", sk, true, 5000);
+        Pair<List<String>, List<Interval>> fwd = reconstruct(background, sk, true, 5000);
 
         log.info("Rev:");
-        Pair<List<String>, List<Interval>> rev = reconstruct("ref", sk, false, 5000);
+        Pair<List<String>, List<Interval>> rev = reconstruct(background, sk, false, 5000);
     }
 
+    private Pair<List<String>, List<Interval>> reconstruct(String background, String sk, boolean goForward, int limit) {
+        List<String> vertices = new ArrayList<>();
+        List<Interval> loci = new ArrayList<>();
+
+        Interval ci = null;
+        int distanceFromNovel = 0;
+        boolean onRef = false;
+        boolean positiveStrand = false;
+
+        while (distanceFromNovel < limit) {
+            if (ci == null) {
+                CortexRecord cr = GRAPH.findRecord(new CortexKmer(sk));
+                Map<Integer, Set<String>> aks = goForward ? TraversalEngine.getAllNextKmers(cr, !sk.equals(cr.getKmerAsString())) : TraversalEngine.getAllPrevKmers(cr, !sk.equals(cr.getKmerAsString()));
+                Set<String> achi = aks.get(GRAPH.getColorForSampleName(CHILD));
+
+                if (achi.size() == 1) {
+                    distanceFromNovel = 0;
+                    sk = achi.iterator().next();
+
+                    Set<Interval> acis = LOOKUPS.get(background).findKmer(sk);
+                    ci = acis.size() == 1 ? acis.iterator().next() : null;
+
+                    if (goForward) {
+                        vertices.add(sk);
+                        loci.add(ci);
+                    } else {
+                        vertices.add(0, sk);
+                        loci.add(0, ci);
+                    }
+                    log.info("{} {}", vertices.get(vertices.size() - 1), loci.get(loci.size() - 1));
+
+                    if (ci != null) {
+                        onRef = true;
+                        positiveStrand = ci.isPositiveStrand();
+                    }
+                } else {
+                    log.info("lastNovel onRef={} achi.size()={}", onRef, achi.size());
+                    break;
+                }
+            } else {
+                do {
+                    Interval aci = positiveStrand ? new Interval(ci.getContig(), ci.getStart() + 1, ci.getEnd() + 1, ci.isNegativeStrand(), null) : new Interval(ci.getContig(), ci.getStart() - 1, ci.getEnd() - 1, ci.isNegativeStrand(), null);
+                    String aref = LOOKUPS.get(background).findKmer(aci);
+
+                    CortexRecord cr = GRAPH.findRecord(new CortexKmer(sk));
+                    Map<Integer, Set<String>> aks = goForward ? TraversalEngine.getAllNextKmers(cr, !sk.equals(cr.getKmerAsString())) : TraversalEngine.getAllPrevKmers(cr, !sk.equals(cr.getKmerAsString()));
+                    Set<String> achi = aks.get(GRAPH.getColorForSampleName(CHILD));
+
+                    if (achi.contains(aref)) {
+                        distanceFromNovel++;
+                        sk = aref;
+                        ci = aci;
+
+                        if (goForward) {
+                            vertices.add(sk);
+                            loci.add(ci);
+                        } else {
+                            vertices.add(0, sk);
+                            loci.add(0, ci);
+                        }
+                        log.info("{} {}", vertices.get(vertices.size() - 1), loci.get(loci.size() - 1));
+                    } else {
+                        if (achi.size() == 1) {
+                            distanceFromNovel = 0;
+                            sk = achi.iterator().next();
+                            ci = null;
+
+                            if (goForward) {
+                                vertices.add(sk);
+                                loci.add(ci);
+                            } else {
+                                vertices.add(0, sk);
+                                loci.add(0, ci);
+                            }
+                            log.info("{} {}", vertices.get(vertices.size() - 1), loci.get(loci.size() - 1));
+                        } else {
+                            log.info("firstNovel onRef={} achi.size()={}", onRef, achi.size());
+                            break;
+                        }
+
+                        onRef = false;
+                    }
+                } while (onRef);
+            }
+        }
+    }
+
+    /*
     private Pair<List<String>, List<Interval>> reconstruct(String background, String sk, boolean goForward, int limit) {
         List<String> vertices = new ArrayList<>();
         List<Interval> loci = new ArrayList<>();
@@ -275,6 +274,7 @@ public class CallNAHRs extends Module {
 
         return new Pair<>(vertices, loci);
     }
+    */
 
     private String recordToString(CortexRecord cr, int childColor, List<Integer> parentColors) {
         List<Object> pieces = new ArrayList<>();
