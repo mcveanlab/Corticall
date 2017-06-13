c84ded37837bbcaa5183b97454cabaed42072412
Kiran V Garimella <kiran@well.ox.ac.uk>
2017-06-13 18:31:01 +0100

    Testing NAHR caller

--- public/java/src/uk/ac/ox/well/indiana/commands/caller/call/CallNAHRs.java
+++ public/java/src/uk/ac/ox/well/indiana/commands/caller/call/CallNAHRs.java
@@ -80,6 +80,8 @@ public class CallNAHRs extends Module {
             if (!used.get(ck)) {
                 String sk = ck.getKmerAsString();
 
+                log.info("{}", sk);
+
                 Pair<List<String>, List<Interval>> recon0 = reconstruct("ref", sk);
                 Pair<List<String>, List<Interval>> recon1 = reconstruct("HB3", sk);
 
@@ -105,7 +107,6 @@ public class CallNAHRs extends Module {
                     pm.update();
                 }
 
-                log.info("{}", sk);
                 log.info("  - {} {} {}", recon0.getFirst().size(), novels0, aggregatedIntervals0);
                 log.info("  - {} {} {}", recon1.getFirst().size(), novels1, aggregatedIntervals1);
             }
