c784913b0ecc93899a1689febe6e2d35e7aaa312
Kiran V Garimella <kiran@well.ox.ac.uk>
2017-06-21 13:35:29 +0100

    Yet another attempt at an NAHR caller, this time using links
    Former-commit-id: 4b69fd5c0c21fccf18a02d56f0afd3c5c549bf01

--- public/java/src/uk/ac/ox/well/indiana/commands/caller/call/CallNahrEvents.java
+++ public/java/src/uk/ac/ox/well/indiana/commands/caller/call/CallNahrEvents.java
@@ -105,8 +105,8 @@ public class CallNahrEvents extends Module {
                         rMatcher.group(2).length() >= 5 && fMatcher.group(2).length() >= 5 &&
                         rMatcher.group(1).length() >= 2 && fMatcher.group(1).length() >= 2) {
                         log.info("    candidate: {} {}", rr.getCortexKmer(), key);
-                        log.info("    - rContigCount: {} {} {}", rMatcher.group(3), rMatcher.group(2).length(), rContigCount);
-                        log.info("    - fContigCount: {} {} {}", fMatcher.group(3), fMatcher.group(2).length(), fContigCount);
+                        log.info("    - rContigCount: .={} {}={} {}", rMatcher.group(1).length(), rMatcher.group(3), rMatcher.group(2).length(), rContigCount);
+                        log.info("    - fContigCount: .={} {}={} {}", fMatcher.group(1).length(), fMatcher.group(3), fMatcher.group(2).length(), fContigCount);
 
                         candidates.add(rr.getCortexKmer());
                     }
