7a2cca25b7709c4346dc31a4f959f3e50de124f6
Kiran V Garimella <kiran@well.ox.ac.uk>
2017-06-27 13:05:21 +0100

    Better logging and formatting
    Former-commit-id: 996d5baa4b72f608bb8cd4050e3b7d051d94dc0d

--- public/java/src/uk/ac/ox/well/indiana/commands/caller/call/AnnotateContigs.java
+++ public/java/src/uk/ac/ox/well/indiana/commands/caller/call/AnnotateContigs.java
@@ -51,7 +51,7 @@ public class AnnotateContigs extends Module {
         List<Integer> recruitColors = GRAPH.getColorsForSampleNames(new ArrayList<>(LOOKUPS.keySet()));
         int refColor = GRAPH.getColorForSampleName("ref");
 
-        out.println(Joiner.on("\t").join("name", "sk", "ck", "cov_" + CHILD, "cov_" + Joiner.on("\tcov_").join(PARENTS), "cov_" + Joiner.on("\tcov_").join(LOOKUPS.keySet()), "cov_ref", "is_novel", "is_filled_gap", "is_recovered_kmer", Joiner.on("\t").join(LOOKUPS.keySet())));
+        out.println(Joiner.on("\t").join("name", "index", "sk", "ck", "cov_" + CHILD, "cov_" + Joiner.on("\tcov_").join(PARENTS), "cov_" + Joiner.on("\tcov_").join(LOOKUPS.keySet()), "cov_ref", "is_novel", "is_filled_gap", "is_recovered_kmer", Joiner.on("\t").join(LOOKUPS.keySet())));
 
         List<ReferenceSequence> rseqs = new ArrayList<>();
         ReferenceSequence aseq;
@@ -102,7 +102,7 @@ public class AnnotateContigs extends Module {
                 boolean isFilledGap = cr != null && cr.getCoverage(childColor) == 0;
                 boolean isRecovered = cr != null && cr.getCoverage(childColor) < GRAPH.getColor(childColor).getLowCovSupernodesThreshold();
 
-                out.println(Joiner.on("\t").join(pieces[0], sk, ck, Joiner.on("\t").join(coverages), isNovel, isFilledGap, isRecovered, Joiner.on("\t").join(allIntervals)));
+                out.println(Joiner.on("\t").join(pieces[0], i, sk, ck, Joiner.on("\t").join(coverages), isNovel, isFilledGap, isRecovered, Joiner.on("\t").join(allIntervals)));
             }
 
             pm.update();
