e577d3d5facef44e8df1c02b364e83623db4b6b4
Kiran V Garimella <kiran@well.ox.ac.uk>
2017-06-27 18:45:48 +0100

    Prototype caller

--- public/java/src/uk/ac/ox/well/indiana/commands/caller/call/Call.java
+++ public/java/src/uk/ac/ox/well/indiana/commands/caller/call/Call.java
@@ -6,10 +6,13 @@ import uk.ac.ox.well.indiana.commands.Module;
 import uk.ac.ox.well.indiana.utils.alignment.kmer.KmerLookup;
 import uk.ac.ox.well.indiana.utils.arguments.Argument;
 import uk.ac.ox.well.indiana.utils.io.cortex.graph.CortexGraph;
+import uk.ac.ox.well.indiana.utils.io.table.TableReader;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by kiran on 23/06/2017.
@@ -30,8 +33,8 @@ public class Call extends Module {
     @Argument(fullName="drafts", shortName="d", doc="Drafts")
     public HashMap<String, KmerLookup> LOOKUPS;
 
-    @Argument(fullName="sequences", shortName="s", doc="Contigs")
-    public FastaSequenceFile CONTIGS;
+    @Argument(fullName="annotations", shortName="a", doc="Annotated contigs")
+    public File ANNOTATIONS;
 
     @Override
     public void execute() {
@@ -39,9 +42,14 @@ public class Call extends Module {
         List<Integer> parentColors = GRAPH.getColorsForSampleNames(PARENTS);
         List<Integer> recruitColors = GRAPH.getColorsForSampleNames(new ArrayList<>(LOOKUPS.keySet()));
 
-        ReferenceSequence rseq;
-        while ((rseq = CONTIGS.nextSequence()) != null) {
-            String seq = rseq.getBaseString();
+        loadAnnotations();
+    }
+
+    private void loadAnnotations() {
+        TableReader tr = new TableReader(ANNOTATIONS);
+
+        for (Map<String, String> m : tr) {
+            log.info("m: {}", m);
         }
     }
 }
