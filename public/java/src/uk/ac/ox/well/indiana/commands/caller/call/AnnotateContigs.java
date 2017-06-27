4b681eb4f9048518738a7e37f3fd0d65e3681218
Kiran V Garimella <kiran@well.ox.ac.uk>
2017-06-27 13:22:48 +0100

    Better logging and formatting

--- public/java/src/uk/ac/ox/well/indiana/commands/caller/call/AnnotateContigs.java
+++ public/java/src/uk/ac/ox/well/indiana/commands/caller/call/AnnotateContigs.java
@@ -70,7 +70,7 @@ public class AnnotateContigs extends Module {
             String seq = rseq.getBaseString();
 
             for (int i = 0; i <= seq.length() - GRAPH.getKmerSize(); i++) {
-                String sk = seq.substring(i, i + GRAPH.getKmerSize());
+                String sk = seq.substring(i, i + GRAPH.getKmerSize()).toUpperCase();
                 CortexKmer ck = new CortexKmer(sk);
                 CortexRecord cr = GRAPH.findRecord(ck);
 
