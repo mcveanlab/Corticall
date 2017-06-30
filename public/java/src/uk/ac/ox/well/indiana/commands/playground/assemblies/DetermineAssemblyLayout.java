cebd92f226135cd24d41df154aef03e4508a49fe
Kiran V Garimella <kiran@well.ox.ac.uk>
2017-06-30 13:38:55 +0100

    Use exon placement to determine assembly layout
    Former-commit-id: 9bba6a8380056e9732f1411ae29f8dcc0d39d42d

--- public/java/src/uk/ac/ox/well/indiana/commands/playground/assemblies/DetermineAssemblyLayout.java
+++ public/java/src/uk/ac/ox/well/indiana/commands/playground/assemblies/DetermineAssemblyLayout.java
@@ -64,7 +64,7 @@ public class DetermineAssemblyLayout extends Module {
         SortedSet<Map.Entry<K,V>> sortedEntries = new TreeSet<>(
                 new Comparator<Map.Entry<K,V>>() {
                     @Override public int compare(Map.Entry<K,V> e1, Map.Entry<K,V> e2) {
-                        int res = e1.getValue().compareTo(e2.getValue());
+                        int res = e2.getValue().compareTo(e1.getValue());
                         return res != 0 ? res : 1;
                     }
                 }
