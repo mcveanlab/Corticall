e0a5f05ce708f68ab6d069d3abc78384cb2fe0fd
Kiran V Garimella <kiran@well.ox.ac.uk>
2017-06-30 13:37:31 +0100

    Use exon placement to determine assembly layout
    Former-commit-id: 6bc3e816e1a26130983d817aaeeecd103fe21711

--- public/java/src/uk/ac/ox/well/indiana/commands/playground/assemblies/DetermineAssemblyLayout.java
+++ public/java/src/uk/ac/ox/well/indiana/commands/playground/assemblies/DetermineAssemblyLayout.java
@@ -10,8 +10,7 @@ import uk.ac.ox.well.indiana.utils.containers.ContainerUtils;
 import uk.ac.ox.well.indiana.utils.io.gff.GFF3;
 import uk.ac.ox.well.indiana.utils.io.gff.GFF3Record;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Created by kiran on 30/06/2017.
@@ -57,7 +56,21 @@ public class DetermineAssemblyLayout extends Module {
         }
 
         for (String contigName : freq.keySet()) {
-            log.info("{} {}", contigName, freq.get(contigName));
+            log.info("{} {}", contigName, entriesSortedByValues(freq.get(contigName)));
         }
+    }
+
+    static <K,V extends Comparable<? super V>> SortedSet<Map.Entry<K,V>> entriesSortedByValues(Map<K,V> map) {
+        SortedSet<Map.Entry<K,V>> sortedEntries = new TreeSet<>(
+                new Comparator<Map.Entry<K,V>>() {
+                    @Override public int compare(Map.Entry<K,V> e1, Map.Entry<K,V> e2) {
+                        int res = e1.getValue().compareTo(e2.getValue());
+                        return res != 0 ? res : 1;
+                    }
+                }
+        );
+
+        sortedEntries.addAll(map.entrySet());
+        return sortedEntries;
     }
 }
