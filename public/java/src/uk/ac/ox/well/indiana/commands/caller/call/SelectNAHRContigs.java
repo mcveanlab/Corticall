083dea50a09b2947bc24b85fc71b4f48e5f7af80
Kiran V Garimella <kiran@well.ox.ac.uk>
2017-08-09 12:02:53 +0100

    Write Circos link files for NAHR events

--- public/java/src/uk/ac/ox/well/indiana/commands/caller/call/SelectNAHRContigs.java
+++ public/java/src/uk/ac/ox/well/indiana/commands/caller/call/SelectNAHRContigs.java
@@ -89,6 +89,8 @@ public class SelectNAHRContigs extends Module {
                         }
                     }
                 }
+
+                nahrEventId++;
             }
         }
     }
