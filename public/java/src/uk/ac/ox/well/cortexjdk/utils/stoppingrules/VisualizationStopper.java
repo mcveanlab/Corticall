da82abdb7e79d85ef830d15b813fd2a00501d085
Kiran V Garimella <kiran@well.ox.ac.uk>
2017-09-15 15:32:29 +0200

    Lower allowed visualization depth

--- public/java/src/uk/ac/ox/well/cortexjdk/utils/stoppingrules/VisualizationStopper.java
+++ public/java/src/uk/ac/ox/well/cortexjdk/utils/stoppingrules/VisualizationStopper.java
@@ -13,7 +13,7 @@ import java.util.Set;
 public class VisualizationStopper extends AbstractTraversalStoppingRule<CortexVertex, CortexEdge> {
     @Override
     public boolean hasTraversalSucceeded(CortexVertex cv, boolean goForward, int traversalColor, Set<Integer> joiningColors, int currentTraversalDepth, int currentGraphSize, int numAdjacentEdges, boolean childrenAlreadyTraversed, DirectedWeightedPseudograph<CortexVertex, CortexEdge> previousGraph, DeBruijnGraph rois) {
-        return numAdjacentEdges == 0 || currentTraversalDepth > 5 || currentGraphSize > 500;
+        return numAdjacentEdges == 0 || currentTraversalDepth > 2 || currentGraphSize > 500;
     }
 
     @Override
