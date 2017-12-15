6a6834e84ce78497a0bca812e12a4842f7c5b675
Kiran V Garimella <kiran@well.ox.ac.uk>
2017-12-15 17:34:58 +0000

    Trying a faster version of RemoveOrphans

--- public/java/src/uk/ac/ox/well/cortexjdk/commands/simulate/generators/EvaluateROIs.java
+++ public/java/src/uk/ac/ox/well/cortexjdk/commands/simulate/generators/EvaluateROIs.java
@@ -4,8 +4,6 @@ import uk.ac.ox.well.cortexjdk.commands.Module;
 import uk.ac.ox.well.cortexjdk.utils.arguments.Argument;
 
 public class EvaluateROIs extends Module {
-    @Argument(fullName="")
-
     @Override
     public void execute() {
 
