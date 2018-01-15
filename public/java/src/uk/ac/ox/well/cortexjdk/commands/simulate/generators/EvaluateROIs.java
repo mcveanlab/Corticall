c3f71bba7fdbdb865ff71f92819710a13243de3a
Kiran V Garimella <kiran@well.ox.ac.uk>
2018-01-15 22:27:17 +0000

    Venn

--- public/java/src/uk/ac/ox/well/cortexjdk/commands/simulate/generators/EvaluateROIs.java
+++ public/java/src/uk/ac/ox/well/cortexjdk/commands/simulate/generators/EvaluateROIs.java
@@ -1,7 +1,9 @@
 package uk.ac.ox.well.cortexjdk.commands.simulate.generators;
 
+import com.google.common.base.Joiner;
 import uk.ac.ox.well.cortexjdk.commands.Module;
 import uk.ac.ox.well.cortexjdk.utils.arguments.Argument;
+import uk.ac.ox.well.cortexjdk.utils.arguments.Output;
 import uk.ac.ox.well.cortexjdk.utils.io.graph.cortex.CortexGraph;
 import uk.ac.ox.well.cortexjdk.utils.io.graph.cortex.CortexRecord;
 import uk.ac.ox.well.cortexjdk.utils.io.table.TableReader;
@@ -11,6 +13,7 @@ import uk.ac.ox.well.cortexjdk.utils.traversal.TraversalEngine;
 import uk.ac.ox.well.cortexjdk.utils.traversal.TraversalEngineFactory;
 
 import java.io.File;
+import java.io.PrintStream;
 import java.util.*;
 
 import static uk.ac.ox.well.cortexjdk.utils.traversal.TraversalEngineConfiguration.GraphCombinationOperator.OR;
@@ -22,6 +25,9 @@ public class EvaluateROIs extends Module {
 
     @Argument(fullName="rois", shortName="r", doc="ROIs")
     public CortexGraph ROIS;
+
+    @Output
+    public PrintStream out;
 
     @Override
     public void execute() {
@@ -51,20 +57,28 @@ public class EvaluateROIs extends Module {
 
         int uniqueToRois = 0, shared = 0, uniqueToKnowns = 0;
 
+        out.println(Joiner.on(" ").join("class", "ck", "id", "numKmers", "index", "kmer", "found"));
+
         for (CanonicalKmer ck : allRois) {
             if (!knownRois.containsKey(ck)) {
                 log.info("uniqueToRois   {}", ck);
                 uniqueToRois++;
+
+                out.println(Joiner.on(" ").join("uniqueToRois", ck, null, null, null, null, null));
             } else if (found.containsKey(ck) && found.get(ck)) {
                 Map<String, String> te = knownRois.get(ck);
 
                 log.info("shared         {} {} {} {} {} {}", ck, te.get("id"), te.get("numKmers"), te.get("index"), te.get("kmer"), found.get(ck));
                 shared++;
+
+                out.println(Joiner.on(" ").join("shared", ck, te.get("id"), te.get("numKmers"), te.get("index"), te.get("kmer"), found.get(ck)));
             } else {
                 Map<String, String> te = knownRois.get(ck);
 
                 log.info("uniqueToKnowns {} {} {} {} {} {}", ck, te.get("id"), te.get("numKmers"), te.get("index"), te.get("kmer"), found.get(ck));
                 uniqueToKnowns++;
+
+                out.println(Joiner.on(" ").join("uniqueToKnowns", ck, te.get("id"), te.get("numKmers"), te.get("index"), te.get("kmer"), found.get(ck)));
             }
         }
 
